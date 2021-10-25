e2f87658cd0473e85a12c61ed1152bca261f069c
awyaki <awyakitaka@gmail.com>
2021-10-25 12:09:52 +0900

    Adjust layout of Header

--- src/renderer/components/Header/style/container.ts
+++ src/renderer/components/Header/style/container.ts
@@ -3,8 +3,8 @@ import { colors } from '~/styleConfig/colors';
 
 export const container: CSSObject = {
   width: '100%',
-  height: '70px',
   display: 'flex',
+  padding: '16px 5% 16px 5%',
   justifyContent: 'space-between',
   borderBottom: `1px solid ${colors.gray.LIGHT}`,
 };
