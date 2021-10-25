e45a8af0b6cbffc6f9aa50a2657861358c893fa9
awyaki <awyakitaka@gmail.com>
2021-10-25 11:42:02 +0900

    Adjust layout of titles

--- src/renderer/pages/components/Header/style/container.ts
+++ src/renderer/pages/components/Header/style/container.ts
@@ -5,5 +5,6 @@ export const container: CSSObject = {
   width: '100%',
   height: '70px',
   display: 'flex',
+  justifyContent: 'space-between',
   borderBottom: `1px solid ${colors.gray.LIGHT}`,
 };
--- src/renderer/pages/components/Header/style/titles.ts
+++ src/renderer/pages/components/Header/style/titles.ts
@@ -3,5 +3,6 @@ import { CSSObject } from '@emotion/react';
 export const titles: CSSObject = {
   display: 'flex',
   width: '730px',
+  marginTop: '32px',
   justifyContent: 'space-between',
 };
