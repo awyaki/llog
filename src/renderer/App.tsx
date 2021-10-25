8c0b0a3f2eb4bbf853a50040c76081df97706d5d
awyaki <awyakitaka@gmail.com>
2021-10-25 10:43:22 +0900

    Create files for Contents page and Header

---
+++ src/renderer/App.tsx
@@ -0,0 +1,9 @@
+import { Contents } from './pages/Contents';
+
+export const App = () => {
+  return (
+    <div>
+      <Contents />
+    </div>
+  );
+};
---
+++ src/renderer/pages/components/Header/index.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <h2>This is Header</h2>
+    </header>
+  );
+};
