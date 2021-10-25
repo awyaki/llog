83b6c25665a605fee7f7ccec4f8d3c2be925404f
awyaki <awyakitaka@gmail.com>
2021-10-25 14:57:03 +0900

    Fix typo

--- src/renderer/pages/style/container.ts
+++ src/renderer/pages/style/container.ts
@@ -2,7 +2,7 @@ import { CSSObject } from '@emotion/react';
 
 export const container: CSSObject = {
   width: '90%',
-  height: 'clac(100% - 90px)',
+  height: 'calc(100% - 90px)',
   margin: '0 auto',
 };
 
