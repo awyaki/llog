36ed788c91f272b60e17ef45fb5836d399b69bce
awyaki <awyakitaka@gmail.com>
2021-10-25 11:44:22 +0900

    Chage default text color from black to gray.

--- src/renderer/style/container.ts
+++ src/renderer/style/container.ts
@@ -1,8 +1,10 @@
 import { CSSObject } from '@emotion/react';
+import { colors } from '~/styleConfig/colors';
 
 export const container: CSSObject = {
   width: '100%',
   height: '100vh',
   minWidth: '600px',
   minHeight: '400px',
+  color: colors.text,
 };
