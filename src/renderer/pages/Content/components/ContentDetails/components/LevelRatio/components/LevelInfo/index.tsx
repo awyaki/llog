8ac21ad7c7eacd5f4f50696dea76190905d75b9e
awyaki <awyakitaka@gmail.com>
2021-10-28 11:45:26 +0900

    Create veiw of LevelInfo
    ブロックのうち特定のレベルのブロックが占める割合を表示するコンポーネント

---
+++ src/renderer/pages/Content/components/ContentDetails/components/LevelRatio/components/LevelInfo/index.tsx
@@ -0,0 +1,18 @@
+import { VFC } from 'react';
+
+import { Block } from './components/Block';
+import { container } from './style/container';
+
+type Props = {
+  level: number;
+  ratio: string;
+};
+
+export const LevelInfo: VFC<Props> = ({ level, ratio }) => {
+  return (
+    <div css={container}>
+      <Block level={level} />
+      <div>{ratio}</div>
+    </div>
+  );
+};
