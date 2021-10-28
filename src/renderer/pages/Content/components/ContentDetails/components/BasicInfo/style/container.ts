939682e4c28883c3ccfbc3c4c0270a99d4ac559e
awyaki <awyakitaka@gmail.com>
2021-10-28 10:42:45 +0900

    Create view of BasicInfo

---
+++ src/renderer/pages/Content/components/ContentDetails/components/BasicInfo/style/container.ts
@@ -0,0 +1,10 @@
+import { CSSObject } from '@emotion/react';
+
+export const container: CSSObject = {
+  '> li': {
+    marginBottom: '8px',
+  },
+  '> li:nth-last-of-type(1)': {
+    marginBottom: '0',
+  }
+};
