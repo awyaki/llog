6f011353a93a824efe5b119bd2bbfb12642194b9
awyaki <awyakitaka@gmail.com>
2021-11-01 17:15:32 +0900

    Add onClick prop

--- src/renderer/pages/Content/components/ContentDetails/components/OverviewBlocksButton/index.tsx
+++ src/renderer/pages/Content/components/ContentDetails/components/OverviewBlocksButton/index.tsx
@@ -1,14 +1,15 @@
-import { VFC } from 'react';
+import { VFC, MouseEventHandler } from 'react';
 
 import { OverviewBlocksButtonIcon } from './components/OverviewBlocksButtonIcon';
 
 type Props = {
   active: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const OverviewBlocksButton: VFC<Props> = ({ active }) => {
+export const OverviewBlocksButton: VFC<Props> = ({ active, onClick }) => {
   return (
-    <button>
+    <button onClick={onClick}>
       <OverviewBlocksButtonIcon active={active} />
     </button>
   );
