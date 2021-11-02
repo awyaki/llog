2d7159f8030747b1d8b70eefe9fb5ddb4f7ba17b
awyaki <awyakitaka@gmail.com>
2021-11-02 17:59:20 +0900

    Create view of BlocksForDetailView

---
+++ src/renderer/pages/Content/components/ContentBlocks/components/BlocksDetailView/components/BlocksContainer/components/SubContainer/components/BlocksForDetailView/index.tsx
@@ -0,0 +1,17 @@
+import { VFC } from 'react';
+
+import { makeContainer } from './style/container';
+
+
+type Props = {
+  level: number;
+  unitNumber: number;
+};
+
+export const BlocksForDetailView: VFC<Props> = ({ level, unitNumber }) => {
+  return (
+    <div css={makeContainer(level)}><span>{unitNumber}</span></div>
+  );
+};
+
+
---
+++ src/renderer/pages/Content/components/ContentBlocks/components/BlocksDetailView/components/BlocksContainer/components/SubContainer/components/BlocksForDetailView/style/container.ts
@@ -0,0 +1,47 @@
+import { CSSObject } from '@emotion/react';
+import { colors } from '~/styleConfig/colors';
+import { font } from '~/styleConfig/font';
+
+
+export const makeContainer = (level: number): CSSObject => {
+  const color = colors.cyan.LEVEL[level];
+
+  const base: CSSObject = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    width: '36px',
+    height: '36px',
+    borderWidth: '1px',
+    fontSize: font.size.S,
+    textAlign: 'center',
+  };
+
+  const zero: CSSObject = {
+    ...base,
+    borderWidth: '3px',
+    borderStyle: 'dashed',
+    borderColor: colors.gray.DEFAULT,
+  };
+
+  const one: CSSObject = {
+    ...base,
+    borderStyle: 'solid',
+    borderColor: colors.gray.DEFAULT,
+    backgroundColor: colors.gray.LIGHT,
+    color: colors.text,
+  };
+
+
+  const others: CSSObject = {
+    ...base,
+    borderStyle: 'solid',
+    borderColor: color,
+    backgroundColor: color,
+    color: colors.white,
+  };
+
+  if (level === 0) return zero;
+  if (level === 1) return one;
+  return others;
+};
