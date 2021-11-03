e515f85f5d6bc12e2069748e49232afdbc0c0e00
awyaki <awyakitaka@gmail.com>
2021-11-03 21:49:08 +0900

    Create view of EditNoteButton

---
+++ src/renderer/pages/NotesOfContent/components/NoteList/components/Note/components/EditNoteButton/index.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+import { EditNoteButtonIcon } from './components/EditNoteButtonIcon';
+
+export const EditNoteButton: VFC = () => {
+  return ( 
+    <button>
+      <EditNoteButtonIcon />
+    </button>
+  );
+};
