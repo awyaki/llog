344f349f6f7dc2960492b33c48f422fc70803bd2
awyaki <awyakitaka@gmail.com>
2021-11-03 21:48:44 +0900

    Create view of DeleteNoteButton

---
+++ src/renderer/pages/NotesOfContent/components/NoteList/components/Note/components/DeleteNoteButton/index.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+import { DeleteNoteButtonIcon } from './components/DeleteNoteButtonIcon';
+
+export const DeleteNoteButton: VFC = () => {
+  return (
+    <button>
+      <DeleteNoteButtonIcon />
+    </button>
+  );
+};
