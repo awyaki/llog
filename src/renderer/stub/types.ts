85e1455fcee00945c288dad1f57b29f6310458ef
awyaki <awyakitaka@gmail.com>
2021-11-03 20:08:21 +0900

    Add Note stub type

--- src/renderer/stub/types.ts
+++ src/renderer/stub/types.ts
@@ -1,5 +1,13 @@
 export type Block = {
-  id: number,
-  level: number,
-  unitNumber: number,
+  id: number;
+  level: number;
+  unitNumber: number;
+};
+
+export type Note = {
+  id: number;
+  modefiedAt: Date;
+  blocks: Block[];
+  tags: { id: number, name: string }[];
+  html: string;
 };
