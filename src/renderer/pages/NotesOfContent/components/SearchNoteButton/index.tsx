04d5cb6ddbcab4c3a0bc09d2eddd1a8f0a1eed58
awyaki <awyakitaka@gmail.com>
2021-11-03 19:51:38 +0900

    Create view of SearchNoteButton

---
+++ src/renderer/pages/NotesOfContent/components/SearchNoteButton/index.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+import { SearchNoteButtonIcon } from './components/SearchNoteButtonIcon';
+
+export const SearchNoteButton = () => {
+  return (
+    <button>
+      <SearchNoteButtonIcon />
+    </button>
+  );
+};
