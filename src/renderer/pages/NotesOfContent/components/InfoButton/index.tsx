557ae6c02f8adbccaec778b10ce11eb3badcbab6
awyaki <awyakitaka@gmail.com>
2021-11-03 18:04:39 +0900

    Create view of InfoButton

---
+++ src/renderer/pages/NotesOfContent/components/InfoButton/index.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+import { InfoButtonIcon } from './components/InfoButtonIcon';
+
+export const InfoButton: VFC = () => {
+  return (
+    <button>
+      <InfoButtonIcon />
+    </button>
+  );
+};
