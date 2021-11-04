344fb0df8c65cedf9bcb5f6247d80331ed239d12
awyaki <awyakitaka@gmail.com>
2021-11-04 09:05:30 +0900

    Create view of TagList

---
+++ src/renderer/pages/NotesOfContent/components/NoteList/components/Note/components/TagList/components/Tag/index.tsx
@@ -0,0 +1,13 @@
+import { VFC } from 'react';
+
+import { container } from './style/container';
+
+type Props = {
+  name: string;
+};
+
+export const Tag: VFC<Props> = ({ name }) => {
+  return (
+    <div css={container}>{name}</div>
+  );
+};
