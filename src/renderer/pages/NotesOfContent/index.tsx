1e6c162a973e5c53ff34da7349111a5ec5afeeff
awyaki <awyakitaka@gmail.com>
2021-11-12 17:12:06 +0900

    fix layout of NotesOfContent

--- src/renderer/pages/NotesOfContent/index.tsx
+++ src/renderer/pages/NotesOfContent/index.tsx
@@ -1,35 +1,55 @@
-import { VFC } from 'react';
+import { VFC, useState, MouseEventHandler } from 'react';
 
 import { container } from './style/container'
 import { title } from './style/title';
 import { buttons } from './style/buttons';
 import { noteLists } from './style/noteLists';
+import { main as mainStyle } from './style/main';
 
 import { TagList } from './components/TagList';
 import { InfoButton } from './components/InfoButton';
 import { CreateNoteButton } from './components/CreateNoteButton'
 import { SearchNoteButton } from './components/SearchNoteButton';
 import { NoteList } from './components/NoteList';
+import { SearchConditions } from './components/SearchConditions';
 
 
 import { notes } from '~/stub/notes';
+import { SearchCondition } from '~/stub/types';
+
+const searchConditions: SearchCondition[] = [
+  { isVaild: true, kind: 'NOTE', type: 'IS', op: 'AND', not: false, text: 'チューリング機械' },
+  { isVaild: true, kind: 'TAG', type: 'IS', op: 'OR', not: true, text: 'インタープリタ' },
+];
 
 export const NotesOfContent: VFC = () => {
+  const [showConditions, setShowConditions] = useState(false);
+
   const contentName = 'コンパイラ原理と構造';
   const tags = [{ id: 1, name: 'Computer Science' }, { id: 2, name: '計算機科学' }];
+  
+  const handleSearchNoteButtonClick: MouseEventHandler<HTMLButtonElement> = () => {
+    setShowConditions((prev) => !prev);
+  };
+
   return (
     <div css={container}>
-      <h1 css={title}>{contentName}</h1>
-      <TagList tags={tags} />
-      <ul css={buttons}>
-        <li><InfoButton /></li>
-        <li><CreateNoteButton /></li>
-        <li><SearchNoteButton /></li>
-      </ul>
-      <ul css={noteLists}>
-        <li><NoteList notes={notes} /></li>
-        <li><NoteList notes={notes} /></li>
-      </ul>
+      <main css={showConditions ? mainStyle : {}}>
+        <h1 css={title}>{contentName}</h1>
+        <TagList tags={tags} />
+        <ul css={buttons}>
+          <li><InfoButton /></li>
+          <li><CreateNoteButton /></li>
+          <li><SearchNoteButton onClick={handleSearchNoteButtonClick} /></li>
+        </ul>
+        <ul css={noteLists}>
+          <li><NoteList notes={notes} /></li>
+          <li><NoteList notes={notes} /></li>
+        </ul>
+      </main>
+      {showConditions ? <SearchConditions 
+                          conditions={searchConditions} 
+                          handleSearchNoteButtonClick={handleSearchNoteButtonClick}/> : undefined}
     </div>
   );
 };
