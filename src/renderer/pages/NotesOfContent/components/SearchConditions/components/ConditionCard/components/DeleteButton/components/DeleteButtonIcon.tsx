0bfea4b85eb22d4af703732896e4400b6243620d
awyaki <awyakitaka@gmail.com>
2021-11-12 11:05:08 +0900

    Create view of DeleteButton

---
+++ src/renderer/pages/NotesOfContent/components/SearchConditions/components/ConditionCard/components/DeleteButton/components/DeleteButtonIcon.tsx
@@ -0,0 +1,25 @@
+import { VFC } from 'react';
+
+export const DeleteButtonIcon: VFC = () => {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="コンテンツ" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="コンテンツ_ノート表示_条件追加サイドバー展開" transform="translate(-1388.000000, -283.000000)">
+                <g id="Group-26" transform="translate(1064.000000, 160.000000)">
+                    <g id="Group-25" transform="translate(20.000000, 28.000000)">
+                        <g id="Group-20" transform="translate(8.000000, 95.000000)">
+                            <g id="Group-4" transform="translate(296.000000, 0.000000)">
+                                <circle id="Oval" stroke="#F70E20" cx="12" cy="12" r="11.5"></circle>
+                                <g id="close_black_24dp-(1)" transform="translate(4.000000, 4.000000)">
+                                    <polygon id="Path" points="0 0 16 0 16 16 0 16"></polygon>
+                                    <polygon id="Path" fill="#F70E20" fillRule="nonzero" points="13.3333333 3.74095238 12.2590476 2.66666667 8 6.92571429 3.74095238 2.66666667 2.66666667 3.74095238 6.92571429 8 2.66666667 12.2590476 3.74095238 13.3333333 8 9.07428571 12.2590476 13.3333333 13.3333333 12.2590476 9.07428571 8"></polygon>
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </svg>
+  );
+};
---
+++ src/renderer/pages/NotesOfContent/components/SearchConditions/components/ConditionCard/components/DeleteButton/index.tsx
@@ -0,0 +1,11 @@
+import { VFC } from 'react';
+
+import { DeleteButtonIcon } from './components/DeleteButtonIcon';
+
+export const DeleteButton: VFC = () => {
+  return (
+    <button>
+      <DeleteButtonIcon />
+    </button>
+  );
+};
