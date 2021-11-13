7c08baf036869b31049089500349d5306d435f81
awyaki <awyakitaka@gmail.com>
2021-11-13 17:56:14 +0900

    Adjust layout of SearchConditions
    SearchCondition componentを画面の左端に接するように配置

--- src/renderer/pages/NotesOfContent/style/container.ts
+++ src/renderer/pages/NotesOfContent/style/container.ts
@@ -4,6 +4,7 @@ import { container as base } from '~/pages/style/container';
 
 export const container: CSSObject = {
   ...base,
+  position: 'relative',
   display: 'flex',
   justifyContent: 'space-between',
 };
