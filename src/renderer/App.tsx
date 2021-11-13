82519eb04bd4b9e7101a7c1ef79b9e76a854db24
awyaki <awyakitaka@gmail.com>
2021-11-13 16:56:47 +0900

    Support modal mode

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,17 @@
+import { VFC, useContext } from 'react';
+import { IsExpandModalProvider, IsExpandModalContext } from './components/IsExpandModalProvider';
 import { Header } from './components/Header';
 import { container } from './style/container';
 import { NotesOfContent } from './pages/NotesOfContent';
 
-export const App = () => {
+export const App: VFC = () => {
+  const { isExpandModal } = useContext(IsExpandModalContext);
   return (
-    <div css={container}>
-      <Header />
-      <NotesOfContent />
-    </div>
+    <IsExpandModalProvider>
+      <div css={{ ...container, overflow: isExpandModal ? 'hidden' : 'visible' }}>
+        <Header />
+        <NotesOfContent />
+      </div>
+    </IsExpandModalProvider>
   );
 };
