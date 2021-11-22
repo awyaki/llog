fb85f5a4899ae2e5fa0a51a4b929ba33f7307689
awyaki <awyakitaka@gmail.com>
2021-11-22 14:17:13 +0900

    fix bug

--- src/renderer/pages/NotesOfContent/hooks/modalChangeSubjectReducer.ts
+++ src/renderer/pages/NotesOfContent/hooks/modalChangeSubjectReducer.ts
@@ -2,6 +2,17 @@ import { Reducer } from 'react';
 import { Condition } from '../types';
 import { State } from './useConditions';
 
+import { Subject, Predicate } from '../types';
+
+const defaultPredicate = (subject: Subject): Predicate => {
+  switch(subject) {
+    case 'Date':
+      return 'IS SINCE';
+    default:
+      return 'IS';
+  }
+};
+
 type Action = {
   id: number;
   newSubject: Condition['subject'];
@@ -11,10 +22,10 @@ export const modalChangeSubjectReducer: Reducer<State, Action> = (state, action)
   const { createConditions, currentConditions } = state;
   const newCurrentConditions = [...currentConditions];
   const index = createConditions.findIndex(({ id }) => action.id === id);
-  const { subject, ...rest } = createConditions[index]
+  const { subject, predicate, ...rest } = createConditions[index]
   const newCreateCondtion = createConditions
                               .slice(0, index)
-                              .concat({ subject: action.newSubject, ...rest })
+                              .concat({ subject: action.newSubject, predicate: defaultPredicate(action.newSubject), ...rest })
                               .concat(createConditions.slice(index+1));
   return {
     createConditions: newCreateCondtion,
