f36702af39c958b8bac81ed2e46c2396a0453fd6
awyaki <awyakitaka@gmail.com>
2021-11-22 16:35:55 +0900

    fix bugs

--- src/renderer/pages/NotesOfContent/hooks/drawerCreateReducer.ts
+++ src/renderer/pages/NotesOfContent/hooks/drawerCreateReducer.ts
@@ -4,9 +4,9 @@ export const drawerCreateReducer = (state: State): State => {
   const { currentConditions, createConditions } = state;
   const newId = currentConditions.map((condition) => condition.id).reduce((acc, cur) => Math.max(acc, cur), 0) + 1;
   const newCurrentConditions = currentConditions.concat([...createConditions]
-                                  .map(({ id, ...rest}) => { 
+                                  .map(({ id, ...rest}, i) => { 
                                         return { 
-                                          id: newId,
+                                          id: newId + i,
                                           isValid: true, 
                                           ...rest }
                                   }));
