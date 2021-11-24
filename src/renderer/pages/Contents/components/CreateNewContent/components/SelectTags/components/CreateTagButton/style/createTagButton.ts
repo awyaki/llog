de6f90099d2b8e1110be393fc101feb761df033c
awyaki <awyakitaka@gmail.com>
2021-11-24 22:31:28 +0900

    Fix style of CreateTagButton

--- src/renderer/pages/Contents/components/CreateNewContent/components/SelectTags/components/CreateTagButton/style/createTagButton.ts
+++ src/renderer/pages/Contents/components/CreateNewContent/components/SelectTags/components/CreateTagButton/style/createTagButton.ts
@@ -6,6 +6,9 @@ import { colors } from '~/styleConfig/colors';
 
 export const createTagButton: CSSObject = {
   ...base,
+  display: 'flex',
+  justifyContent: 'center',
+  padding: '5px 10px',
   border: `1px dashed ${colors.text}`,
   marginBottom: '10px',
 };
