c1f85b39ffd395041a315b72cc0fac36932bf22b
awyaki <awyakitaka@gmail.com>
2021-11-24 17:29:29 +0900

    Put Contents components on App component to develop this App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,13 @@
 import { VFC } from 'react';
 import { Header } from './components/Header';
-import { NotesOfContent } from './pages/NotesOfContent';
+import { Contents } from './pages/Contents';
 import { Box } from '@chakra-ui/react';
 
 export const App: VFC = () => {
   return (
     <Box>
       <Header />
-      <NotesOfContent />
+      <Contents />
     </Box>
   );
 };
