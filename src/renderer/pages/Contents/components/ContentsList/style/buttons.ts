999ca4394766873ba98c43964700e551b89600da
awyaki <awyakitaka@gmail.com>
2021-11-24 21:36:04 +0900

    Delete files

--- src/renderer/pages/Contents/components/ContentsList/style/buttons.ts
+++
@@ -1,8 +0,0 @@
-import { CSSObject } from '@emotion/react';
-
-export const buttons: CSSObject = {
-  display: 'flex',
-  width: '120px',
-  justifyContent: 'space-between',
-  marginBottom: '32px',
-};
