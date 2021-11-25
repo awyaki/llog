43e6646a6da17bbe8112a774dbce040098cd009e
awyaki <awyakitaka@gmail.com>
2021-11-25 21:13:49 +0900

    register getAllTag function on renderer process

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,1 +1,7 @@
-export {};
+import { ipcRenderer, contextBridge } from 'electron';
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  createTag: (name: string) => ipcRenderer.invoke('createTag', name),
+  getAllTag: () => ipcRenderer.invoke('getAllTag'),
+});
+
