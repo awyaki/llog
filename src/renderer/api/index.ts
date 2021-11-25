eafdbb4e9e1c8f48a429ed54624ff3fd74e27dd8
awyaki <awyakitaka@gmail.com>
2021-11-25 21:16:33 +0900

    Create getAllTag funtion for renderer process

--- src/renderer/api/index.ts
+++ src/renderer/api/index.ts
@@ -1,3 +1,7 @@
 export const createTag = async (name: string) => {
   return await window.electronAPI.createTag(name);
 };
+
+export const getAllTag = async () => {
+  return await window.electronAPI.getAllTag();
+};
