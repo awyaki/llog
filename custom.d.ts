53fd878763fcc9d8e4d42480b299405deb8fd4b0
awyaki <awyakitaka@gmail.com>
2021-11-25 19:41:49 +0900

    Add createTag type

---
+++ custom.d.ts
@@ -0,0 +1,14 @@
+import { Prisma, Tag } from '@prisma/client';
+
+
+
+interface IElectronAPI {
+  createTag: (name: string) => Prisma.Prisma__TagClient<Tag>;
+}
+
+declare global {
+  interface Window {
+    electronAPI: IElectronAPI;
+  }
+}
+
