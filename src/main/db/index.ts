f094b420032368538477082f96f258092883b461
awyaki <awyakitaka@gmail.com>
2021-11-25 21:12:19 +0900

    Register getAllTag function

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -1,10 +1,18 @@
 import { ipcMain } from 'electron';
+
 import { create as createTag } from './api/tag/create';
+import { getAll as getAllTag } from './api/tag/getAll';
 
 export const useDBQueryOnClient = () => {
   ipcMain.handle('createTag', async (_, name: string) => {
     const result = await createTag(name);
     return result;
   });
+
+  ipcMain.handle('getAllTag', async () => {
+    const result = await getAllTag();
+    return result;
+  });
 };
+
 
