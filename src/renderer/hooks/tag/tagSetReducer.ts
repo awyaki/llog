5aabb4b27475f854b98054d2dda92e82edc01574
awyaki <awyakitaka@gmail.com>
2021-11-26 19:50:48 +0900

    Create tagSetReducer

---
+++ src/renderer/hooks/tag/tagSetReducer.ts
@@ -0,0 +1,16 @@
+import { Tag } from '@prisma/client';
+
+import { State } from './types';
+
+type Action = {
+  type: 'APP/SET_TAG';
+  tags: Tag[];
+};
+
+export const tagSetReducer = (nextState: State): State => {
+  return nextState;
+};
+
+
+export type { Action as SetTagAction };
+
