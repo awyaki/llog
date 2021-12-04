b612775d70ac9a026b8c038ba8e4e75622a2732c
awyaki <awyakitaka@gmail.com>
2021-12-04 21:08:13 +0900

    Create create function for a content creation

---
+++ src/main/db/api/content/create.ts
@@ -0,0 +1,21 @@
+import { Tag, Block } from '@prisma/client';
+import { prisma } from '../../db';
+
+export const create = async (
+  name: string,
+  tags: Tag[], 
+  blocks: Block[],
+) => {
+  const result = prisma.content.create({
+    data: {
+      name: name,
+      tags: {
+        connect: [...tags.map(({ id }) => { return { id: id } })],
+      },
+      blocks: {
+        create: [...blocks], 
+      },
+    },
+  });
+  return result;
+};
