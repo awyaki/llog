095dd9d4df02f967c053160773cb7de7ff51de05
awyaki <awyakitaka@gmail.com>
2021-12-04 21:43:09 +0900

    Add createContent and getAllContent

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -1,7 +1,11 @@
 import { ipcRenderer, contextBridge } from 'electron';
 
+import { Tag, Block } from '@prisma/client';
+
 contextBridge.exposeInMainWorld('electronAPI', {
   createTag: (name: string) => ipcRenderer.invoke('createTag', name),
   getAllTag: () => ipcRenderer.invoke('getAllTag'),
+  createContnet: (name: string, tags: Tag[], blocks: Block[]) => ipcRenderer.invoke('createContent', name, tags, blocks),
+  getAllContent: () => ipcRenderer.invoke('getAllContent'),
 });
 
