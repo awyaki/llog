95fbc82edf6e66551c5971ad36b8d97d426366de
awyaki <awyakitaka@gmail.com>
2021-12-04 21:40:00 +0900

    Add createContent and getAllContent

--- src/main/db/index.ts
+++ src/main/db/index.ts
@@ -1,7 +1,12 @@
 import { ipcMain } from 'electron';
 
+import { Tag, Block } from '@prisma/client';
+
 import { create as createTag } from './api/tag/create';
 import { getAll as getAllTag } from './api/tag/getAll';
+
+import { create as createContent } from './api/content/create';
+import { getAll as getAllContent } from './api/content/getAll';
 
 export const useDBQueryOnClient = () => {
   ipcMain.handle('createTag', async (_, name: string) => {
@@ -11,6 +16,16 @@ export const useDBQueryOnClient = () => {
 
   ipcMain.handle('getAllTag', async () => {
     const result = await getAllTag();
+    return result;
+  });
+
+  ipcMain.handle('createContent', async (_, name: string, tags: Tag[], blocks: Block[]) => {
+    const result = await createContent(name, tags, blocks);
+    return result;
+  });
+
+  ipcMain.handle('getAllContent', async () => {
+    const result = await getAllContent();
     return result;
   });
 };
