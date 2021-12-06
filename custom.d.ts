ab1cf01595171869dbde07068ae326cc51062796
awyaki <awyakitaka@gmail.com>
2021-12-06 15:48:50 +0900

    Add types for manupulating contents data

--- custom.d.ts
+++ custom.d.ts
@@ -1,10 +1,12 @@
-import { Prisma, Tag } from '@prisma/client';
+import { Prisma, Tag, Content } from '@prisma/client';
 
 
 
 interface IElectronAPI {
   createTag: (name: string) => Prisma.Prisma__TagClient<Tag>;
   getAllTag: () => Prisma.Prisma__TagClient<Tag[]>;
+  createContent: (name: string, tags: Tag[], blockNumber: number) => Prisma.Prisma__ContentClient<Content>;
+  getAllContent: () => Prisma.Prisma__TagClient<Content[]>;
 }
 
 declare global {
