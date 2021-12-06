5f487342acfd856cbc9711deffdb1be51c58e32f
awyaki <awyakitaka@gmail.com>
2021-12-06 18:45:45 +0900

    Fix for getting nested data

--- src/main/db/api/content/getAll.ts
+++ src/main/db/api/content/getAll.ts
@@ -1,6 +1,12 @@
 import { prisma } from '../../db';
 
 export const getAll = async () => {
-  const result = await prisma.content.findMany();
+  const result = await prisma.content.findMany({
+    include: { 
+      tags: true,
+      notes: true,
+      blocks: true,
+    },
+  });
   return result;
 };
