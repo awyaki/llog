a794b3417873982ddc837f9f2d1438ad6c7e6c74
awyaki <awyakitaka@gmail.com>
2021-12-07 21:18:56 +0900

    Remove console.log

--- src/renderer/pages/Contents/components/ContentsList/index.tsx
+++ src/renderer/pages/Contents/components/ContentsList/index.tsx
@@ -6,7 +6,6 @@ import { list } from './style/list';
 
 export const ContentsList: VFC = () => {
   const contents = useContentsList();
-  console.log('ContentsList', contents);
   return (
     <ul css={list}>
       {contents.map(({ id, name }) => <li key={id}>{name}</li>)}
