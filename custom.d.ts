d91c77d6fedebc356688ec568be6c30a94f02e7a
awyaki <awyakitaka@gmail.com>
2021-12-08 14:27:48 +0900

    fix type

--- custom.d.ts
+++ custom.d.ts
@@ -7,7 +7,7 @@ interface IElectronAPI {
   getAllTag: () => Prisma.Prisma__TagClient<Tag[]>;
   createContent: (name: string, tags: Tag[], blockNumber: number) => Prisma.Prisma__ContentClient<Content>;
   getAllContent: () => Prisma.Prisma__ContentClient<Content[]>;
-  getContent: (id: number) => Prisma__ContentClient<Content>;
+  getContent: (id: number) => Prisma.Prisma__ContentClient<Content>;
 }
 
 declare global {
