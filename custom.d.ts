ea7b13250c8e49cb75e74dcdb003f7a60868e407
awyaki <awyakitaka@gmail.com>
2021-12-08 16:31:07 +0900

    Fix type

--- custom.d.ts
+++ custom.d.ts
@@ -1,13 +1,19 @@
 import { Prisma, Tag, Content } from '@prisma/client';
 
-
+const contentWithRelation = Prisma.validator<Prisma.ContentArgs>()({
+  include: {
+    tags: true,
+    blocks: true,
+    notes: true,
+  },
+});
 
 interface IElectronAPI {
   createTag: (name: string) => Prisma.Prisma__TagClient<Tag>;
   getAllTag: () => Prisma.Prisma__TagClient<Tag[]>;
   createContent: (name: string, tags: Tag[], blockNumber: number) => Prisma.Prisma__ContentClient<Content>;
   getAllContent: () => Prisma.Prisma__ContentClient<Content[]>;
-  getContent: (id: number) => Prisma.Prisma__ContentClient<Content | null>;
+  getContent: (id: number) => Prisma.Prisma__ContentClient<Prisma.ContentGetPayload<typeof contentWithRelation> | null>;
 }
 
 declare global {
