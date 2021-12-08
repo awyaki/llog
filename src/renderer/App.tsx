49c78202a0bf3e0faf3409ff0a787e93c72ca530
awyaki <awyakitaka@gmail.com>
2021-12-08 13:33:12 +0900

    Add router for page transition

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,13 +1,20 @@
 import { VFC } from 'react';
 import { Header } from './components/Header';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import { Contents } from './pages/Contents';
 import { Box } from '@chakra-ui/react';
 
 export const App: VFC = () => {
   return (
-    <Box>
-      <Header />
-      <Contents />
-    </Box>
+    <HashRouter>
+      <Box>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <Contents />
+          </Route>
+        </Switch>
+      </Box>
+    </HashRouter>
   );
 };
