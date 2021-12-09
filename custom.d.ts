a3c7cfcdc73cc5325c6cc934b56e7d729cb98c79
awyaki <awyakitaka@gmail.com>
2021-12-09 15:32:19 +0900

    Set type for createNote

--- custom.d.ts
+++ custom.d.ts
@@ -1,4 +1,4 @@
-import { Prisma, Tag, Content } from '@prisma/client';
+import { Prisma, Tag, Content, Note } from '@prisma/client';
 
 const contentWithRelation = Prisma.validator<Prisma.ContentArgs>()({
   include: {
@@ -8,12 +8,14 @@ const contentWithRelation = Prisma.validator<Prisma.ContentArgs>()({
   },
 });
 
+
 interface IElectronAPI {
   createTag: (name: string) => Prisma.Prisma__TagClient<Tag>;
   getAllTag: () => Prisma.Prisma__TagClient<Tag[]>;
   createContent: (name: string, tags: Tag[], blockNumber: number) => Prisma.Prisma__ContentClient<Content>;
   getAllContent: () => Prisma.Prisma__ContentClient<Content[]>;
   getContent: (id: number) => Prisma.Prisma__ContentClient<Prisma.ContentGetPayload<typeof contentWithRelation> | null>;
+  createNote: (mkd: string, transformed: string, tags: Tag[], blocks: Block[], contentId: number) => Prisma.Prisma__NoteClient<Note>
 }
 
 declare global {
