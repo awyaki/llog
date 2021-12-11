5b577b3b5291c8e70d84313209c14587f840c394
awyaki <awyakitaka@gmail.com>
2021-12-11 16:17:26 +0900

    Remove Icon conponent

--- src/renderer/pages/CreateNote/components/InfoButton/components/InfoButtonIcon.tsx
+++ src/renderer/pages/CreateNote/components/InfoButton/components/InfoButtonIcon.tsx
@@ -1,21 +1,18 @@
 import { VFC } from 'react';
-import { Icon } from '@chakra-ui/react';
 
 export const InfoButtonIcon: VFC = () => {
   return (
-    <Icon>
-      <svg width="51px" height="51px" viewBox="0 0 51 51" version="1.1" xmlns="http://www.w3.org/2000/svg">
-          <g id="ノート追加編集" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-              <g id="ノート追加画面" transform="translate(-100.000000, -200.000000)">
-                  <g id="Group-3" transform="translate(100.000000, 200.000000)">
-                      <circle id="Oval" stroke="#0590A7" cx="25.5" cy="25.5" r="25"></circle>
-                      <text id="Info." fontFamily="HiraginoSans-W4, Hiragino Sans" fontSize="10" fontWeight="normal" fill="#0590A7">
-                          <tspan x="15" y="29">Info.</tspan>
-                      </text>
-                  </g>
-              </g>
-          </g>
-      </svg>
-    </Icon>
+    <svg width="51px" height="51px" viewBox="0 0 51 51" version="1.1" xmlns="http://www.w3.org/2000/svg">
+        <g id="ノート追加編集" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="ノート追加画面" transform="translate(-100.000000, -200.000000)">
+                <g id="Group-3" transform="translate(100.000000, 200.000000)">
+                    <circle id="Oval" stroke="#0590A7" cx="25.5" cy="25.5" r="25"></circle>
+                    <text id="Info." fontFamily="HiraginoSans-W4, Hiragino Sans" fontSize="10" fontWeight="normal" fill="#0590A7">
+                        <tspan x="15" y="29">Info.</tspan>
+                    </text>
+                </g>
+            </g>
+        </g>
+    </svg>
   );
 };
