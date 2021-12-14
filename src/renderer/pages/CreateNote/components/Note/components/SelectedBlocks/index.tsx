25825eea89a809b3262ab22a4691576b3df896f7
awyaki <awyakitaka@gmail.com>
2021-12-14 10:11:07 +0900

    Add CSSObject prop to SelectedBlocks

--- src/renderer/pages/CreateNote/components/Note/components/SelectedBlocks/index.tsx
+++ src/renderer/pages/CreateNote/components/Note/components/SelectedBlocks/index.tsx
@@ -1,5 +1,7 @@
 import { VFC } from 'react';
 
+import { Box, CSSObject } from '@chakra-ui/react';
+
 import { Block as BlockType } from '@prisma/client';
 
 import { Block } from './components/Block';
@@ -8,12 +10,14 @@ import { container } from './style/container';
 
 type Props = {
   blocks: BlockType[];
-};
+} & CSSObject;
 
-export const SelectedBlocks: VFC<Props> = ({ blocks }) => {
+export const SelectedBlocks: VFC<Props> = ({ blocks, ...CSSObject }) => {
   return (
-    <ul css={container}>
-      {blocks.map(({ id, level, unitNumber }) => <li key={id}><Block level={level} unitNumber={unitNumber} /></li>)}
-    </ul>
+    <Box __css={CSSObject}>
+      <ul css={container}>
+        {blocks.map(({ id, level, unitNumber }) => <li key={id}><Block level={level} unitNumber={unitNumber} /></li>)}
+      </ul>
+    </Box>
   );
 };
