0d241c5858cb6e5ea06c5ceafc2bcf91a6c12b67
awyaki <awyakitaka@gmail.com>
2021-12-14 21:50:38 +0900

    Adjust Header and Pages layout.

--- src/renderer/components/Header/style/container.ts
+++ src/renderer/components/Header/style/container.ts
@@ -2,11 +2,11 @@ import { CSSObject } from '@emotion/react';
 import { colors } from '~/styleConfig/colors';
 
 export const container: CSSObject = {
+  position: 'fixed',
   width: '100%',
   height: '90px',
   display: 'flex',
   padding: '16px 5%',
   justifyContent: 'space-between',
   borderBottom: `1px solid ${colors.gray.LIGHT}`,
-  marginBottom: '32px',
 };
--- src/renderer/pages/style/container.ts
+++ src/renderer/pages/style/container.ts
@@ -2,6 +2,7 @@ import { CSSObject } from '@emotion/react';
 
 export const container: CSSObject = {
   width: '90%',
+  paddingTop: '132px',
   margin: '0 auto',
 };
 
