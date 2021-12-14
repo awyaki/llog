179604477c0d3e6f090aa0ecc43a8ff1688cc19a
awyaki <awyakitaka@gmail.com>
2021-12-14 09:34:12 +0900

    Adjust the layout

--- src/renderer/pages/CreateNote/CreateNote.tsx
+++ src/renderer/pages/CreateNote/CreateNote.tsx
@@ -1,20 +1,29 @@
 import { VFC } from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, HStack } from '@chakra-ui/react';
 
 import { InfoButton } from './components/InfoButton';
 import { ShowNoteButton } from './components/ShowNotesButton';
 import { Note } from './components/Note';
 
+
 import { container } from '~/pages/style/container';
+
 
 export const CreateNote: VFC = () => {
 
+  const contentNameStub = 'コンパイラ原理と構造';
+
   return (
     <Box css={container}>
-      <InfoButton />
-      <ShowNoteButton />
-      <Note />
+      <HStack>
+        <Box width="55vw">
+          <Heading as="h2" size="lg">{contentNameStub}</Heading>
+          <InfoButton />
+          <ShowNoteButton />
+          <Note />
+        </Box>
+      </HStack>
     </Box>
   );
 };
