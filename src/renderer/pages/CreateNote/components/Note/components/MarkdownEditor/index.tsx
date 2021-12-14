b99987cea8bad2637d8d67e4f61abcf3ce20aa9f
awyaki <awyakitaka@gmail.com>
2021-12-14 20:32:37 +0900

    Remove vertical scroll of the Markdown editor.

--- src/renderer/pages/CreateNote/components/Note/components/MarkdownEditor/index.tsx
+++ src/renderer/pages/CreateNote/components/Note/components/MarkdownEditor/index.tsx
@@ -1,4 +1,4 @@
-import { VFC, useRef, useState } from 'react';
+import { VFC, useRef, useState, useEffect } from 'react';
 import AceEditor from 'react-ace';
 
 
@@ -18,25 +18,46 @@ export const MarkdownEditor: VFC<Props> = ({ width, height }) => {
   const [markdown, setMarkdown] = useState('');
   const [mode, setMode] = useState<'vim' | ''>('');
   const editorRef = useRef<AceEditor>(null);
+
   const handleChange = (value: string) => {
+    console.log("value was changed");
     setMarkdown(value);
   };
-  
+
+  useEffect(() => {
+    const aceEditor = editorRef.current;
+    if (aceEditor != null) {
+      const lines = aceEditor.editor.getSession().getScreenLength();
+      const lineHeight = (document.querySelector('.ace_line') as HTMLDivElement)
+                          .style
+                          .height
+                          .replace('px', '');
+      console.log('MarkdownEditor', lines, lineHeight);
+      const newHeight = lines * Number(lineHeight);
+      console.log('newHeight', newHeight);
+      aceEditor.editor.container.style.height = `${newHeight}px`;
+      aceEditor.editor.resize();
+    }
+
+  }, [markdown]);
+
   return (
-      <Box css={{ ...container, height: height, width: width }}>
+      <Box __css={{ ...container, height: height, width: width }}>
         <AceEditor
           ref={editorRef}
           mode="markdown"
           theme="github"
           onChange={handleChange}
-          width={"100%"}
+          width="100%"
+          height="100px"
           tabSize={2}
-          fontSize={18}
+          fontSize={16}
           showPrintMargin={true}
           showGutter={false}
           highlightActiveLine={false}
           value={markdown}
           wrapEnabled={true}
+          setOptions={{ animatedScroll: false, highlightActiveLine: true }}
           keyboardHandler={mode}
         />
       </Box>
