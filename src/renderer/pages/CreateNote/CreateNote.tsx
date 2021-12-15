4db186e56cc2c41f8a4dd68aa3abc7e780f0a131
awyaki <awyakitaka@gmail.com>
2021-12-15 16:09:46 +0900

    Create Header for CreateNote

--- src/renderer/pages/CreateNote/CreateNote.tsx
+++ src/renderer/pages/CreateNote/CreateNote.tsx
@@ -2,6 +2,7 @@ import { VFC } from 'react';
 
 import { Box, Heading, HStack } from '@chakra-ui/react';
 
+import { Header } from './components/Header';
 import { InfoButton } from './components/InfoButton';
 import { ShowNoteButton } from './components/ShowNotesButton';
 import { Note } from './components/Note';
@@ -15,17 +16,20 @@ export const CreateNote: VFC = () => {
   const contentNameStub = 'コンパイラ原理と構造';
 
   return (
-    <Box css={container}>
-      <HStack>
-        <Box width="55vw">
-          <Heading as="h2" size="lg" mb="16px">{contentNameStub}</Heading>
-          <HStack width="120px" mb="16px">
-            <InfoButton />
-            <ShowNoteButton />
-          </HStack>
-          <Note />
-        </Box>
-      </HStack>
-    </Box>
+    <>
+      <Header isNoteChange={true} />
+      <Box css={container}>
+        <HStack>
+          <Box width="55vw">
+            <Heading as="h2" size="lg" mb="16px">{contentNameStub}</Heading>
+            <HStack width="120px" mb="16px">
+              <InfoButton />
+              <ShowNoteButton />
+            </HStack>
+            <Note />
+          </Box>
+        </HStack>
+      </Box>
+    </>
   );
 };
--- src/renderer/pages/CreateNote/components/Header/index.tsx
+++ src/renderer/pages/CreateNote/components/Header/index.tsx
@@ -1,8 +1,16 @@
 import { VFC } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Header as DefaultHeader } from '~/pages/Header';
 
-type Props = {};
+type Props = {
+  isNoteChange: boolean;
+};
 
-export const Header: VFC<Props> = () => {
-  return (<></>);
+export const Header: VFC<Props> = ({ isNoteChange }) => {
+  const confirmer = () => {
+    const message = `Do you really want to leave the page? 
+                    Every text which isn't saved will be clear.`
+    return confirm(message);
+  };
+
+  return <DefaultHeader confirmer={isNoteChange ? confirmer : undefined}/>;
 };
