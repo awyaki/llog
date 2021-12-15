2999ea853638b61ceca6ab1d8663878842b13633
awyaki <awyakitaka@gmail.com>
2021-12-15 20:50:01 +0900

    Wrap CreateNote with ContentContextProvider

--- src/renderer/pages/HandleNote/index.tsx
+++ src/renderer/pages/HandleNote/index.tsx
@@ -1,10 +1,13 @@
 import { VFC } from 'react';
 
+import { ContentContextProvider } from '../ContentContextProvider';
 
 import { CreateNote as SubCreateNote } from './CreateNote';
 
 export const CreateNote: VFC = () => {
   return (
-      <SubCreateNote />
+      <ContentContextProvider>
+        <SubCreateNote />
+      </ContentContextProvider>
   );
 };
