c83f04a00a0c5b74074e24e3cffbced893de066a
awyaki <awyakitaka@gmail.com>
2021-12-15 22:54:26 +0900

    Create Block

---
+++ src/renderer/pages/HandleNote/components/SelectBlocks/components/Block/index.tsx
@@ -0,0 +1,15 @@
+import { VFC } from 'react';
+
+import { Block as BlockType } from '@prisma/client';
+
+import { makeContainer } from '~/pages/style/block';
+
+type Props = Pick<BlockType, 'level' | 'unitNumber'>;
+
+export const Block: VFC<Props> = ({ level, unitNumber }) => {
+  return (
+    <button css={makeContainer(level)}>
+      {unitNumber}
+    </button>
+  );
+};
