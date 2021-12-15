3c50e710821f6d9fb7069e7ccbf09a713df08185
awyaki <awyakitaka@gmail.com>
2021-12-15 09:35:16 +0900

    Create MarkdownPreview page

--- src/renderer/pages/CreateNote/components/Note/components/MarkdownPreview/index.tsx
+++ src/renderer/pages/CreateNote/components/Note/components/MarkdownPreview/index.tsx
@@ -1,8 +1,9 @@
-import { VFC, createElement } from 'react';
+import { VFC, useEffect, useState } from 'react';
 
 import { Box } from '@chakra-ui/react';
 import { container } from './style/container';
 
+import { markdownToHTML } from '~/api';
 import 'zenn-content-css';
 
 type Props = {
@@ -10,10 +11,18 @@ type Props = {
 };
 
 export const MarkdownPreview: VFC<Props> = ({ markdown }) => {
+  const [html, setHtml] = useState('');
+
+  useEffect(() => {
+    (async () => {
+      const _html = await markdownToHTML(markdown);
+      setHtml(_html);
+    })();
+  }, [markdown]);
 
   return (
-    <Box __css={container} className="znc">
-      <h1>Hoge</h1>
+    <Box __css={container}>
+      <div className="znc" dangerouslySetInnerHTML={{ __html: html }}></div>
     </Box>
   );
 };
