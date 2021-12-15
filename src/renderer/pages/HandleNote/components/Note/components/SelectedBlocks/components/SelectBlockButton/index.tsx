2153d68ddba3aa273c0aca7aec5eb476aa982dd1
awyaki <awyakitaka@gmail.com>
2021-12-15 21:30:34 +0900

    Create SelectBlockButton

--- src/renderer/pages/HandleNote/components/Note/components/SelectedBlocks/components/SelectBlockButton/index.tsx
+++ src/renderer/pages/HandleNote/components/Note/components/SelectedBlocks/components/SelectBlockButton/index.tsx
@@ -1,12 +1,12 @@
 import { VFC } from 'react';
+import { colors } from '~/styleConfig/colors';
 
-import { SelectBlockButtonIcon } from './components/SelectBlockButtonIcon';
-
+import { MdOutlineModeEditOutline } from 'react-icons/md';
 
 export const SelectBlockButton: VFC = () => {
   return (
-    <button>
-      <SelectBlockButtonIcon />
+    <button css={{ width: '36px', height: '36px', border: `2px dashed ${colors.cyan.DEFAULT}`, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <MdOutlineModeEditOutline size="27px" color={colors.cyan.DEFAULT} />
     </button>
   );
 };
