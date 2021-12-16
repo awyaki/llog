ef9dda6267d9f1d5029791d6fc23a25ddd7c4f98
awyaki <awyakitaka@gmail.com>
2021-12-16 12:54:27 +0900

    Create SelectedBlocksContextProvider

---
+++ src/renderer/pages/HandleNote/SelectedBlocksContextProvider/index.tsx
@@ -0,0 +1,19 @@
+import { FC, createContext, Dispatch } from 'react';
+import { Block } from '@prisma/client';
+
+import { useSlectedBlocks, Action } from '../hooks/useSelectedBlocks';
+
+type SlectedBlocksContextType = {
+  blocks: Block[];
+  dispatch: Dispatch<Action>;
+};
+
+const SlectedBlocksContext = createContext<SlectedBlocksContextType>({ blocks: [], dispatch: () => {} });
+
+export const SelectedBlocksContextProvider: FC = () => {
+  const [selectedBlocks, dispatch] = useSlectedBlocks();
+  return (
+    <SlectedBlocksContext.Provider value={{ blocks: selectedBlocks, dispatch: dispatch }}>
+    </SlectedBlocksContext.Provider>
+  );
+};
---
+++ src/renderer/pages/HandleNote/hooks/useSelectedBlocks.ts
@@ -0,0 +1,24 @@
+import { useReducer, Reducer } from 'react';
+import { Block } from '@prisma/client';
+
+type State = Block[];
+
+export type Action = {
+  type: 'SELECTED_BLOCKS/TOGGLE';
+  block: Block;
+};
+
+const reduer: Reducer<State, Action> = (state, action) => {
+  switch (action.type) {
+    case 'SELECTED_BLOCKS/TOGGLE': {
+      const index = state.findIndex(({ id }) => id === action.block.id);
+      const nextState = index === -1 
+              ? state.concat(action.block)
+              : state.slice(0, index).concat(state.slice(index + 1));
+      return nextState.sort((a, b) => a.unitNumber - b.unitNumber);
+    }
+  }
+};
+
+
+export const useSlectedBlocks = () => useReducer(reduer, []);
