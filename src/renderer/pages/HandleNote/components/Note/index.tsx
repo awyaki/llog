2b7694c2e3cfec7a2fcf8ea38c72abd356d170a6
awyaki <awyakitaka@gmail.com>
2021-12-16 17:26:16 +0900

    Add CSSObject to Note props

--- src/renderer/pages/HandleNote/components/Note/index.tsx
+++ src/renderer/pages/HandleNote/components/Note/index.tsx
@@ -1,18 +1,19 @@
 import { VFC, useState } from 'react';
 
-import { Box, HStack } from '@chakra-ui/react';
+import { Box, HStack, CSSObject } from '@chakra-ui/react';
 
 import { MarkdownEditor } from './components/MarkdownEditor';
 import { MarkdownPreview } from './components/MarkdownPreview';
 import { ShowSaveDate } from './components/ShowSaveDate';
 
+type Props = CSSObject;
 
-export const Note: VFC = () => {
+export const Note: VFC<Props> = (props) => {
   const [markdown, setMarkdown] = useState(''); 
   const [mode, setMode] = useState<'edit' | 'preview'>('edit'); 
 
   return (
-    <Box>
+    <Box __css={props}>
       <HStack justifyContent="center"  mb="16px">
         <ShowSaveDate notSaved={true} date={new Date()}/>
       </HStack>
