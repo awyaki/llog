ef04978134310ddd285fca024c3050c0ce1a0018
awyaki <awyakitaka@gmail.com>
2021-12-16 17:10:03 +0900

    Create title style

---
+++ src/renderer/pages/HandleNote/components/ControlBox/style/title.ts
@@ -0,0 +1,8 @@
+import { CSSObject } from '@chakra-ui/react';
+import { colors } from '~/styleConfig/colors';
+import { font } from '~/styleConfig/font';
+
+export const title: CSSObject = {
+  fontSize: font.size.S, 
+  color: colors.text,
+};
