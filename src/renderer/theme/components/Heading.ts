3a76ccdc5675811c14f4e191bb36af480a1b82c2
awyaki <awyakitaka@gmail.com>
2021-12-16 21:53:52 +0900

    Add the variant to Heading

---
+++ src/renderer/theme/components/Heading.ts
@@ -0,0 +1,14 @@
+import { SystemStyleObject } from '@chakra-ui/theme-tools';
+import { colors } from '~/styleConfig/colors';
+import { font } from '~/styleConfig/font';
+
+const h2: SystemStyleObject = {
+  color: colors.text,
+  fontSize: font.size.M,
+};
+
+export const Heading = {
+  variants: {
+    'h2': h2,
+  },
+};
--- src/renderer/theme/index.ts
+++ src/renderer/theme/index.ts
@@ -1,11 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
-import { Button } from './components/Button';
+import { Button, Heading } from './components';
 import { colors } from './colors/colors';
 
 const overrides = {
   components: {
     Button,
+    Heading,
   },
   colors,
 };
