3fb055a7904f3d1a85e43bb36e5968bd159d4106
awyaki <awyakitaka@gmail.com>
2021-12-16 14:31:04 +0900

    fix style

--- src/renderer/pages/HandleNote/components/Note/components/SelectedBlocks/components/Block/index.tsx
+++ src/renderer/pages/HandleNote/components/Note/components/SelectedBlocks/components/Block/index.tsx
@@ -2,8 +2,6 @@ import { VFC } from 'react';
 
 import { Block as BlockType } from '@prisma/client';
 
-import { Box } from '@chakra-ui/react';
-
 import { makeContainer } from '~/pages/style/block';
 
 
@@ -11,6 +9,6 @@ type Props = Pick<BlockType, 'level' | 'unitNumber'>;
 
 export const Block: VFC<Props> = ({ level , unitNumber }) => {
   return (
-    <div css={makeContainer(level)}><span>{unitNumber}</span></div>
+    <div css={makeContainer(level)}>{unitNumber}</div>
   );
 };
