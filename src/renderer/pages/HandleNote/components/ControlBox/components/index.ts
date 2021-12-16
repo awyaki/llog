c16eebff16ef80f9c88893f778008e6bc8333ea6
awyaki <awyakitaka@gmail.com>
2021-12-16 16:36:58 +0900

    Set import/export

---
+++ src/renderer/pages/HandleNote/components/ControlBox/components/index.ts
@@ -0,0 +1,7 @@
+export { CommitButton } from './CommitButton';
+export { OneMoreNoteButton } from './OneMoreNoteButton';
+export { PreviewButton } from './PreviewButton';
+export { SaveButton } from './SaveButton';
+export { SelectedBlocks } from './SelectedBlocks';
+export { SelectedTags } from './SelectedTags';
+export { TakeANoteButton } from './TakeANoteButton';
