6204f65a8bc87b418518e555991fef0b60b6224a
awyaki <awyakitaka@gmail.com>
2021-12-16 13:03:16 +0900

    fix

--- src/renderer/pages/HandleNote/SelectedBlocksContextProvider/index.tsx
+++ src/renderer/pages/HandleNote/SelectedBlocksContextProvider/index.tsx
@@ -4,16 +4,16 @@ import { Block } from '@prisma/client';
 import { useSlectedBlocks, Action } from '../hooks/useSelectedBlocks';
 
 type SelectedBlocksContextType = {
-  blocks: Block[];
+  selectedBlocks: Block[];
   dispatch: Dispatch<Action>;
 };
 
-export const SelectedBlocksContext = createContext<SelectedBlocksContextType>({ blocks: [], dispatch: () => {} });
+export const SelectedBlocksContext = createContext<SelectedBlocksContextType>({ selectedBlocks: [], dispatch: () => {} });
 
 export const SelectedBlocksContextProvider: FC = () => {
   const [selectedBlocks, dispatch] = useSlectedBlocks();
   return (
-    <SelectedBlocksContext.Provider value={{ blocks: selectedBlocks, dispatch: dispatch }}>
+    <SelectedBlocksContext.Provider value={{ selectedBlocks, dispatch }}>
     </SelectedBlocksContext.Provider>
   );
 };
