72be3f103f03f09d0d915be994248d2cf6573bc8
awyaki <awyakitaka@gmail.com>
2021-12-17 19:44:32 +0900

    Create the logic of OK button on CraateNewTag

--- src/renderer/pages/HandleNote/components/ModalToCreateNewTag/index.tsx
+++ src/renderer/pages/HandleNote/components/ModalToCreateNewTag/index.tsx
@@ -1,4 +1,8 @@
-import { VFC } from 'react';
+import { VFC, useState, useCallback, useContext, ChangeEventHandler } from 'react';
+import { SelectedTagsContext } from '~/pages/HandleNote/SelectedTagsContextProvider';
+
+import { createTag } from '~/api';
+
 import {
   Modal,
   ModalOverlay,
@@ -17,7 +21,20 @@ type Props = Omit<ModalProps, 'children'>;
 export const ModalToCreateNewTag: VFC<Props> = ({ 
   isOpen,
   onClose,
-  }) => {
+}) => {
+  const [inputValue, setInputValue] = useState('');
+  const { dispatch } = useContext(SelectedTagsContext);  
+  
+  const handleChange: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    setInputValue(e.target.value);
+  }, []);
+
+  const handleOKClick = useCallback(async () => {
+    const newTag = await createTag(inputValue);
+    dispatch({ type: 'SELECTED_TAGS/ADD', tag: newTag });
+    onClose();
+  }, [inputValue]);
+
   return (
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
@@ -25,9 +42,13 @@ export const ModalToCreateNewTag: VFC<Props> = ({
           <ModalHeader>New tag</ModalHeader>
             <ModalBody>
               <FormControl>
-                <Input type="text" mb="16px"/>
+                <Input 
+                  type="text" 
+                  mb="16px" 
+                  value={inputValue}
+                  onChange={handleChange} />
                 <HStack justifyContent="flex-end">
-                  <Button>OK</Button>
+                  <Button onClick={handleOKClick}>OK</Button>
                   <Button onClick={onClose}>Cancel</Button>
                 </HStack>
               </FormControl>
