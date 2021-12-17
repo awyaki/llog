6e2a0e3ea87d0cdccc04e2bcc0ec8f83a1e429db
awyaki <awyakitaka@gmail.com>
2021-12-17 17:19:47 +0900

    fix style

--- src/renderer/pages/HandleNote/components/ControlBox/components/SelectedTags/style/container.ts
+++ src/renderer/pages/HandleNote/components/ControlBox/components/SelectedTags/style/container.ts
@@ -4,7 +4,6 @@ import { CSSObject } from '@emotion/react';
 export const container: CSSObject = {
   display: 'flex',
   flexWrap: 'wrap',
-  maxHeight: '50px',
   overflowY: 'scroll',
   '> li': {
     marginRight: '8px',
