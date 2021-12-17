606af4f30bf6111280238a65f3a3f1d1109e2ef5
awyaki <awyakitaka@gmail.com>
2021-12-17 10:09:18 +0900

    Create Tag

---
+++ src/renderer/pages/HandleNote/components/ModalToSelectTags/components/Tag/index.tsx
@@ -0,0 +1,14 @@
+import { VFC, MouseEventHandler } from 'react';
+
+import { container } from './style';
+
+type Props = {
+  name: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const Tag: VFC<Props> = ({ name, onClick }) => {
+  return (
+    <button onClick={onClick} css={container}>{name}</button>
+  );
+};
