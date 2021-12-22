147634cfda76643a58f22e1b17e38d27844adead
awyaki <awyakitaka@gmail.com>
2021-12-22 13:42:07 +0900

    Create noteUpdate function

--- src/main/db/api/note/index.ts
+++ src/main/db/api/note/index.ts
@@ -1,2 +1,3 @@
 export * from './create';
 export * from './get';
+export { update as noteUpdate } from './update';
---
+++ src/main/db/api/note/update.ts
@@ -0,0 +1,34 @@
+import { prisma } from '../../db';
+
+import { Tag, Block } from '@prisma/client';
+
+export const update = async (
+  id: number,
+  markdown: string,
+  html: string,
+  tags: Tag[],
+  blocks: Block[],
+  contentId: number,
+  commitedAt: Date,
+  updatedAt: Date
+) => {
+  const note = await prisma.note.update({
+    where: { id: id },
+    data: {
+      origin: markdown,
+      transformed: html,
+      commitedAt: commitedAt,
+      updatedAt: updatedAt,
+      content: {
+        connect: { id: contentId }
+      },
+      tags: {
+        connect: [...tags.map(({ id }) => ({ id: id }))],
+      },
+      blocks: {
+        connect: [...blocks.map(({ id }) => ({ id: id }))],
+      },
+    },
+  });
+  return note;
+};
