22f655e95d0be8251d4579f4de124c51bbe4f722
awyaki <awyakitaka@gmail.com>
2021-12-22 10:37:25 +0900

    Create createNote function

--- src/renderer/api/index.ts
+++ src/renderer/api/index.ts
@@ -1,3 +1,5 @@
+import { Tag, Block } from '@prisma/client';
+
 export const createTag = async (name: string) => {
   return await window.electronAPI.createTag(name);
 };
@@ -12,6 +14,16 @@ export const getContent = async (id: number) => {
 
 export const getNote = async (id: number) => {
   return await window.electronAPI.getNote(id);
+};
+
+export const createNote = async (
+  markdown: string,
+  html: string,
+  tags: Tag[],
+  blocks: Block[],
+  contentId: number
+) => {
+  return await window.electronAPI.createNote(markdown, html, tags, blocks, contentId);
 };
 
 export const markdownToHTML = async (markdown: string) => {
