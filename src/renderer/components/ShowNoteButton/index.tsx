a17d4deba1d09c9e4affae93fe10ad302a3ddd82
awyaki <awyakitaka@gmail.com>
2021-12-28 18:08:44 +0900

    Create ShowNoteButton

---
+++ src/renderer/components/ShowNoteButton/index.tsx
@@ -0,0 +1,15 @@
+import { VFC, MouseEventHandler } from 'react';
+
+import { ShowNoteIcon } from './components';
+
+type Props = {
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const ShowNoteButton: VFC<Props> = ({ onClick }) => {
+  return (
+    <button onClick={onClick}>
+      <ShowNoteIcon />
+    </button>
+  );
+};
--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -1,3 +1,4 @@
 export * from './NoteWithContentName';
 export * from './CancelButton';
 export * from './EditNoteButton';
+export * from './ShowNoteButton';
