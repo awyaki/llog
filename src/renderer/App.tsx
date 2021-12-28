b05cf1063051c1562f397997a501bb69686bf6f7
awyaki <awyakitaka@gmail.com>
2021-12-28 11:22:24 +0900

    Set routing to develop NoteWithContentName component

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,11 +3,14 @@ import { Header } from './components/Header';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import { Contents } from './pages/Contents';
+
 import { Content } from './pages/Content';
 
 import { NotesOfContent } from './pages/NotesOfContent';
 
 import { CreateNote } from './pages/HandleNote';
+
+import { NoteWithContentName } from './components/NoteWithContentName';
 
 import { Box } from '@chakra-ui/react';
 
@@ -16,6 +19,14 @@ export const App: VFC = () => {
     <HashRouter>
       <Box>
         <Switch>
+          <Route path="/">
+            <NoteWithContentName 
+              contentName="コンパイラ原理と構造"
+              tags={[{ id: 1, name: 'computer', }]}
+              blocks={[{ id: 1, unitNumber: 1, level: 1 }, { id: 2, unitNumber: 2, level: 2 }, { id: 3, unitNumber: 3, level: 3 }]}
+              updatedAt="2021/7/30"
+            />
+          </Route>
           <Route path="/content/:contentId/notes">
             <NotesOfContent />
           </Route>
