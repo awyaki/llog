db8bdd42cfe7467ee971d96fd0560187179e659b
awyaki <awyakitaka@gmail.com>
2021-12-30 10:05:22 +0900

    Create type for OnClickCommit

---
+++ src/renderer/pages/Logs/type.ts
@@ -0,0 +1,11 @@
+import { Log, BlockForLog, TagForLog } from '@prisma/client';
+
+export type OnClickCommit = (
+    markdown: string,
+    html: string,
+    blocks: BlockForLog[],
+    tags: TagForLog[],
+    contentName: string,
+    noteId: number | null,
+    contentId: number | null
+  ) => Promise<Log>;
