d2a68896057a95cc7fe6487042c66f4e1f19cddf
awyaki <awyakitaka@gmail.com>
2021-12-31 20:45:33 +0900

    fix

--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -3,4 +3,4 @@ export * from './CancelButton';
 export * from './EditNoteButton';
 export * from './ShowNoteButton';
 export * from './CommitButton';
-export * from './NotificationMessageContextProvider';
+export * from './NotifierContextProvider';
