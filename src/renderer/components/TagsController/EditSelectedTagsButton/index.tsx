0ddbb82ff1633c5de3c5c2060d2b781c0880d31a
awyaki <awyakitaka@gmail.com>
2022-01-08 18:39:33 +0900

    fix name

--- src/renderer/components/TagsController/EditSelectedTagsButton/index.tsx
+++ src/renderer/components/TagsController/EditSelectedTagsButton/index.tsx
@@ -4,7 +4,7 @@ import { EditIcon } from './components';
 
 import { SelectedTagsContext } from '../SelectedTagsContextProvider';
 
-export const EditSelectedButton: VFC = () => {
+export const EditSelectedTagsButton: VFC = () => {
   const { onOpenModalToSelectTags } = useContext(SelectedTagsContext);
   return (
     <button onClick={onOpenModalToSelectTags}>
