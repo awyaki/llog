c711bfb4dd856aada6de430a6fc66da0b83ca5ef
awyaki <awyakitaka@gmail.com>
2022-01-08 18:34:39 +0900

    fix export

--- src/renderer/components/TagsController/index.ts
+++ src/renderer/components/TagsController/index.ts
@@ -1,1 +1,4 @@
 export * from './ModalToSelectTags';
+export * from './CreateTagButton';
+export * from './EditSelectedTagsButton';
+export * from './ModalToCreateTags';
--- src/renderer/components/index.ts
+++ src/renderer/components/index.ts
@@ -5,3 +5,4 @@ export * from './ShowNoteButton';
 export * from './CommitButton';
 export * from './NotifierContextProvider';
 export * from './Notifier';
+export * from './TagsController';
