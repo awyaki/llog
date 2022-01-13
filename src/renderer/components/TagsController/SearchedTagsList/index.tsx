8412727ac29e7a4be8241e845d0af9b63067ab35
awyaki <awyakitaka@gmail.com>
2022-01-13 23:33:49 +0900

    Change order of buttons
    The buttons are located on the top for consitance of its location.

--- src/renderer/components/TagsController/SearchedTagsList/index.tsx
+++ src/renderer/components/TagsController/SearchedTagsList/index.tsx
@@ -14,10 +14,14 @@ import { SelectedTagsContext } from '../SelectedTagsContextProvider';
 export const SearchedTagsList: VFC = () => {
   const { searchedTags } = useContext(SelectedTagsContext);
   return (
-    <ul css={container}>
-      {searchedTags.map(({ id, name }) => <li key={id}><div css={tagStyle}>{name}</div></li>)}
-      <li><EditSearchedTagsButton /></li>
-      <li><ReleaseSearchedTagsButton /></li>
-    </ul>
+    <>
+      <ul css={{ ...container, marginBottom: '8px' }}>
+        <li><EditSearchedTagsButton /></li>
+        <li><ReleaseSearchedTagsButton /></li>
+      </ul>
+      <ul css={container}>
+        {searchedTags.map(({ id, name }) => <li key={id}><div css={tagStyle}>{name}</div></li>)}
+      </ul>
+    </>
   );
 };
