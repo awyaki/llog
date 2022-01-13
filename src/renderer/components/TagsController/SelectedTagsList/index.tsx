3860a6a04875ab2e3359075103cbfd356d1ee778
awyaki <awyakitaka@gmail.com>
2022-01-13 23:37:53 +0900

    Change orger of buttons
    Previously, the location of buttons were changed by incresing selected tags.

--- src/renderer/components/TagsController/SelectedTagsList/index.tsx
+++ src/renderer/components/TagsController/SelectedTagsList/index.tsx
@@ -2,6 +2,8 @@ import { VFC, useContext } from 'react';
 
 import { EditSelectedTagsButton } from '../EditSelectedTagsButton';
 
+import { CreateTagButton } from '../CreateTagButton';
+
 import {
   container,
   tagStyle
@@ -12,9 +14,14 @@ import { SelectedTagsContext } from '../SelectedTagsContextProvider';
 export const SelectedTagsList: VFC = () => {
   const { selectedTags } = useContext(SelectedTagsContext);
   return (
-    <ul css={container}>
-      {selectedTags.map(({ id, name }) => <li key={id}><div css={tagStyle}>{name}</div></li>)}
-      <li><EditSelectedTagsButton /></li>
-    </ul>
+    <>
+      <ul css={{ ...container, marginBottom: '8px' }}>
+        <li><CreateTagButton /></li>
+        <li><EditSelectedTagsButton /></li>
+      </ul>
+      <ul css={container}>
+        {selectedTags.map(({ id, name }) => <li key={id}><div css={tagStyle}>{name}</div></li>)}
+      </ul>
+    </>
   );
 };
