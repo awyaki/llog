a80282e6a49d78d605a1fcfd59aeaaf44dd93821
awyaki <awyakitaka@gmail.com>
2022-01-13 23:24:45 +0900

    fix

--- src/renderer/components/TagsController/SearchedTagsList/style/container.ts
+++ src/renderer/components/TagsController/SearchedTagsList/style/container.ts
@@ -2,7 +2,7 @@ import { CSSObject } from '@emotion/react';
 
 export const container: CSSObject = {
   display: 'flex',
-  width: '300px',
+  width: '100%',
   flexWrap: 'wrap',
   '> li': {
     marginRight: '4px',
