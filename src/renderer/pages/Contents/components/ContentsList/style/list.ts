5b81c749c440371957a8efe1ac0fd740b28abcf9
awyaki <awyakitaka@gmail.com>
2022-01-13 09:40:07 +0900

    Attach scroll to the container of list of content

--- src/renderer/pages/Contents/components/ContentsList/style/list.ts
+++ src/renderer/pages/Contents/components/ContentsList/style/list.ts
@@ -1,5 +1,8 @@
 import { CSSObject } from '@emotion/react';
 export const list: CSSObject = {
+  height: '65vh',
+  overflowY: 'scroll',
+  padding: '16px',
   '> li': {
     marginBottom: '8px',
   }
