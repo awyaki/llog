ef183dfc7f9a32f1d9b65da4d68a5e94ba3d5bff
awyaki <awyakitaka@gmail.com>
2022-01-17 21:23:31 +0900

    fix

--- src/renderer/pages/PreviewNote/components/TagList/style/container.ts
+++ src/renderer/pages/PreviewNote/components/TagList/style/container.ts
@@ -2,6 +2,7 @@ import { CSSObject } from '@emotion/react';
 
 export const container: CSSObject = {
   display: 'flex',
+  flexWrap: 'wrap',
   '> li': {
     marginRight: '4px',
     marginBottom: '4px',
