de3cee45f340de1a7156c86b868cfad20e1d25bc
awyaki <awyakitaka@gmail.com>
2022-01-17 09:28:53 +0900

    export setting

---
+++ src/renderer/pages/NotesOfContent/components/NoteList/components/Note/components/index.ts
@@ -0,0 +1,6 @@
+export * from './Blocks';
+export * from './BodyOfNote';
+export * from './ModefiedAt';
+export * from './EditNoteButton';
+export * from './PreviewNoteButton';
+export * from './TagList';
