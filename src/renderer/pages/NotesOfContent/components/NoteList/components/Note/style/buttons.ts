60b22fd46915bac3dd459f663f7a2f33f4384821
awyaki <awyakitaka@gmail.com>
2022-01-17 09:39:47 +0900

    Adjust style

--- src/renderer/pages/NotesOfContent/components/NoteList/components/Note/style/buttons.ts
+++ src/renderer/pages/NotesOfContent/components/NoteList/components/Note/style/buttons.ts
@@ -3,4 +3,10 @@ import { CSSObject } from '@emotion/react';
 export const buttons: CSSObject = {
   display: 'flex',
   justifyContent: 'flex-end',
+  '> li': {
+    marginLeft: '4px',
+  },
+  '> li:nth-of-type(1)': {
+    marginLeft: '0',
+  }
 };
