f29603ddf0eac1b56719685df0a5d68f6ebfcbc0
awyaki <awyakitaka@gmail.com>
2022-01-19 13:46:54 +0900

    fix

--- src/renderer/utils/asyncForEach.ts
+++ src/renderer/utils/asyncForEach.ts
@@ -1,2 +1,2 @@
-export const asyncForEach = async <T>(array: T[], fn: (arg: T) => Promise<void>) => 
+export const asyncForEach = async <T>(array: T[], fn: (arg: T) => Promise<any>) => 
   array.reduce((accPromise, cur) => accPromise.then(() => fn(cur)), Promise.resolve());
