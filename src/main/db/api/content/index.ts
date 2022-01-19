f29b3dd8538ef178bb7eea03fc062872339dadcc
awyaki <awyakitaka@gmail.com>
2022-01-19 21:56:57 +0900

    Create upsertContentBlocks

--- src/main/db/api/content/index.ts
+++ src/main/db/api/content/index.ts
@@ -2,3 +2,4 @@ export { create as createContent } from './create';
 export { get as getContent } from './get';
 export { getAll as getAllContent } from './getAll';
 export * from './updateContentName';
+export * from './upsertContentBlocks';
---
+++ src/main/db/api/content/upsertContentBlocks.ts
@@ -0,0 +1,24 @@
+import { prisma } from '../../db';
+
+import { range } from '../../../utils';
+
+export const upsertContentBlocks = async (
+  id: number,
+  blockMaxUnitNumber: number,
+  howManyBlocks: number,
+) => {
+  const result = await prisma.content.update({
+    where: { id: id },
+    data: {
+      blocks: {
+        create: [...range(0, howManyBlocks)].map((_, i) => ({
+          iteration: 0,
+          level: 0,
+          unitNumber: blockMaxUnitNumber + 1 + i
+        })),
+      }
+    },
+  });
+
+  return result;
+};
