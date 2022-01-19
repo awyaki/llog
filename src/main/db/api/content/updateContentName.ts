398309217c15fc1e8fe333b10db2b0a89f10d31d
awyaki <awyakitaka@gmail.com>
2022-01-19 21:34:41 +0900

    Create updateContentName function

---
+++ src/main/db/api/content/updateContentName.ts
@@ -0,0 +1,14 @@
+import { Content } from '@prisma/client';
+import { prisma } from '../../db';
+
+export const updateContentName = async ({ id, name }: Pick<Content, 'id' | 'name'>) => {
+  const result = await prisma.content.update({
+    where: { id: id },
+    data: {
+      name: name
+    }
+  });
+
+  return result;
+};
+
