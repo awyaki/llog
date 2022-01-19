7a43bd2ea6a0fb9e54ddc8dbc640c30c097603d7
awyaki <awyakitaka@gmail.com>
2022-01-19 22:17:07 +0900

    Create updateContentTags

---
+++ src/main/db/api/content/updateContentTags.ts
@@ -0,0 +1,19 @@
+import { Tag } from '@prisma/client';
+import { prisma } from '../../db';
+
+export const updateContentTags = async (
+  id: number,
+  tags: Tag[],
+) => {
+  const result = await prisma.content.update({
+    where: { id: id },
+    data: {
+      tags: {
+        set: [],
+        connect: tags.map(({ id }) => ({ id })),
+      },
+    },
+  });
+
+  return result;
+};
