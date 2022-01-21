b4651a57c0b0f1012e253c2adbb8c6fb136350bc
awyaki <awyakitaka@gmail.com>
2022-01-21 10:49:24 +0900

    Create CancelIcon

---
+++ src/renderer/components/Icons/CancelIcon/index.tsx
@@ -0,0 +1,17 @@
+import { VFC } from 'react';
+
+import { colors } from '~/styleConfig';
+
+import { MdCancel } from 'react-icons/md';
+
+import { Icon } from '@chakra-ui/react';
+
+export const CancelIcon: VFC = () => {
+  return (
+    <Icon 
+      as={MdCancel}
+      w={4}
+      h={4}
+      color={colors.cyan.DEFAULT} />
+  );
+};
