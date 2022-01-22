a04bbac7d115bd2d3a7a1ebbec5338681b5e3435
awyaki <awyakitaka@gmail.com>
2022-01-22 09:03:33 +0900

    trivial fix

--- src/renderer/pages/Content/components/ContentDetails/components/BasicInfo/index.tsx
+++ src/renderer/pages/Content/components/ContentDetails/components/BasicInfo/index.tsx
@@ -15,9 +15,11 @@ export const BasicInfo: VFC<Props> = ({ id, created, blocks, isUpsertBlocksMode 
   return (
     <ul css={container}>
       <li>Created time：{created}</li>
-      <li>Blocks：{isUpsertBlocksMode 
+      <li css={{ display: 'flex' }}>
+        <span>Blocks：</span>
+        {isUpsertBlocksMode 
                     ? <ContentBlocksForm id={id} maxUnitNumber={blocks} /> 
-                    : blocks}</li>
+                    : <span>blocks</span>}</li>
     </ul>
   );
 };
