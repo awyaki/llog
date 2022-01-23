fbfadc0ce423d68fcd2a03fe45028b097a2db5fb
awyaki <awyakitaka@gmail.com>
2022-01-23 11:14:56 +0900

    fix

--- src/renderer/components/DeleteButton/index.tsx
+++ src/renderer/components/DeleteButton/index.tsx
@@ -1,16 +1,19 @@
 import { VFC } from 'react';
 
+import { CSSObject } from '@emotion/react';
+
 import { makeContainer } from './style';
 
 type Props = {
   onClick: () => void;
   disabled: boolean;
+  css: CSSObject;
 };
 
-export const DeleteButton: VFC<Props> = ({ onClick, disabled }) => {
+export const DeleteButton: VFC<Props> = ({ onClick, disabled, ...rest }) => {
   return (
     <button 
-      css={makeContainer(disabled)}
+      css={{ ...makeContainer(disabled), ...rest }}
       onClick={onClick} 
       disabled={disabled}>
       Delete
