cbaf69400a9d837f8f5ba5a865e73c74c4862409
awyaki <awyakitaka@gmail.com>
2022-01-23 17:21:56 +0900

    fix

--- src/main/db/api/content/deleteContent.ts
+++ src/main/db/api/content/deleteContent.ts
@@ -1,9 +1,24 @@
 import { prisma } from '../../db';
 
-export const deleteContent = async (id: number) => {
-  const result = await prisma.content.delete({
+export const deleteContent = async (
+  id: number,
+) => {
+  
+  await prisma.log.deleteMany({
+    where: { contentId: id },
+  });
+
+  await prisma.note.deleteMany({
+    where: { contentId: id },
+  });
+  
+  await prisma.block.deleteMany({
+    where: { contentId: id },
+  });
+
+  const deletedContent = await prisma.content.delete({
     where: { id: id }
   });
 
-  return result;
+  return deletedContent;
 };
