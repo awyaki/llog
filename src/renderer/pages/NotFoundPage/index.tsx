f8154302bcf8b1dbe9daf4fc676837059949695d
awyaki <awyakitaka@gmail.com>
2022-01-24 22:26:26 +0900

    Set up Menu

--- src/renderer/pages/NotFoundPage/index.tsx
+++ src/renderer/pages/NotFoundPage/index.tsx
@@ -1,16 +1,16 @@
 import { VFC } from 'react';
 
-import { Header } from '../Header';
+import { Menu } from '~/components';
 
 import { container } from '~/pages/style';
 
 export const NotFoundPage: VFC = () => {
   return (
-    <>
-      <Header />
+    <div css={{ display: 'flex' }}>
+      <Menu />
       <div css={container}>
         <h1>Page is Not Found</h1>
       </div>
-    </>
+    </div>
   );
 };
