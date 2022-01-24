7875440d03847a8823b514914d061b4283cf329c
awyaki <awyakitaka@gmail.com>
2022-01-24 22:44:09 +0900

    Delete old Header

--- src/renderer/components/Header/style/titles.ts
+++
@@ -1,8 +0,0 @@
-import { CSSObject } from '@emotion/react';
-
-export const titles: CSSObject = {
-  display: 'flex',
-  width: '730px',
-  marginTop: '32px',
-  justifyContent: 'space-between',
-};
