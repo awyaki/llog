f6431ffcc51a02c2298fa933225de5d2e145b73e
awyaki <awyakitaka@gmail.com>
2022-01-25 10:01:05 +0900

    Create NotesButton

---
+++ src/renderer/components/Buttons/NotesButton/index.tsx
@@ -0,0 +1,20 @@
+import { VFC, ButtonHTMLAttributes } from 'react';
+
+import { CSSObject } from '@emotion/react';
+
+import { NotesIcon } from '~/components/Icons';
+
+import { menuButton } from '../style';
+
+type Props = {
+  css?: CSSObject;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const NotesButton: VFC<Props> = ({ ...rest }) => {
+  return (
+    <button 
+      css={menuButton} {...rest}>
+      <NotesIcon />
+    </button>
+  );
+};
