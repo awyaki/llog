e950782ceda290ed096879e4f3718490b64c8125
awyaki <awyakitaka@gmail.com>
2022-01-27 14:52:17 +0900

    Add animation on ContentCard

--- src/renderer/pages/Contents/components/ContentsList/components/ContentCard/index.tsx
+++ src/renderer/pages/Contents/components/ContentsList/components/ContentCard/index.tsx
@@ -6,16 +6,37 @@ import { ContentWithRelation } from '~/pages/type';
 
 import { makeFormalTimeString } from '~/utils';
 
-import { SlideFade } from '@chakra-ui/react';
-
-
 import { 
-  container,
-  contentName,
-  dateStyle,
-  } from './style';
+  colors,
+  font
+} from '~/styleConfig';
+
+
+import {
+  motion,
+  Variants
+} from 'framer-motion';
 
 type Props = Pick<ContentWithRelation, 'name' | 'createdAt' | 'tags'>;
+
+const container: Variants = {
+  normal: {
+    width: '100%',
+    border: `2px solid ${colors.cyan.DEFAULT}`,
+    backgroundColor: colors.cyan.DEFAULT,
+    boxShadow: '1px 1px 12px 1px rgba(0, 0, 0, .3)',
+    borderRadius: '5px',
+    padding: '16px',
+    color: colors.white,
+  },
+  delta: {
+    scale: 1.01,
+    backgroundColor: colors.white,
+    borderColor: colors.cyan.DEFAULT,
+    color: colors.cyan.DEFAULT,
+  },
+};
+
 
 export const ContentCard: VFC<Props> = ({ 
   createdAt, 
@@ -23,13 +44,20 @@ export const ContentCard: VFC<Props> = ({
   name,
 }) => {
   return (
-    <SlideFade in={true} offsetY="30px" css={container} unmountOnExit={true}>
-      <div css={dateStyle}>{makeFormalTimeString(createdAt)}</div>
+    <motion.div 
+      variants={container}
+      initial="normal"
+      whileHover="delta"
+      whileFocus="delta">
+      <div css={{
+        fontSize: font.size.SS,
+        marginBottom: '6px',
+      }}>{makeFormalTimeString(createdAt)}</div>
       <TagsList 
-        css={{ marginBottom: '8px',}}
+        css={{ marginBottom: '8px' }}
         tags={tags}
         secandary />
-      <h2 css={contentName}>{name}</h2>
-    </SlideFade>
+      <h2 css={{ fontSize: font.size.M }}>{name}</h2>
+    </motion.div>
   );
 };
