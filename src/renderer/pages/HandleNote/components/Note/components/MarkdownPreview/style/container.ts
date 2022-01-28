a412ce1ad557b38a6f46866def289c1371f03edb
awyaki <awyakitaka@gmail.com>
2022-01-28 15:04:49 +0900

    fix style

--- src/renderer/pages/HandleNote/components/Note/components/MarkdownPreview/style/container.ts
+++ src/renderer/pages/HandleNote/components/Note/components/MarkdownPreview/style/container.ts
@@ -1,7 +1,6 @@
 import { CSSObject } from '@emotion/react';
 
 export const container: CSSObject = {
-  minHeight: '500px',
   padding: '50px',
   boxShadow: '0px 0px 80px -26px rgba(0, 0, 0, 0.5)',
   borderRadius: '25px',
