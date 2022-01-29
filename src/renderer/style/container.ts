b7dfd65f266eee3954db955a6038fb300ff59f29
awyaki <awyakitaka@gmail.com>
2022-01-29 09:20:11 +0900

    fix

--- src/renderer/style/container.ts
+++ src/renderer/style/container.ts
@@ -3,7 +3,6 @@ import { colors } from '~/styleConfig/colors';
 
 export const container: CSSObject = {
   width: '100%',
-  height: '100vh',
   minWidth: '800px',
   minHeight: '400px',
   color: colors.text,
