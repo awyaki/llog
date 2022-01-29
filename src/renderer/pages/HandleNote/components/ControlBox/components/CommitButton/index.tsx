fc0d72ec253bc04416f2789f648897516f43df25
awyaki <awyakitaka@gmail.com>
2022-01-29 13:20:43 +0900

    Add css prop

--- src/renderer/pages/HandleNote/components/ControlBox/components/CommitButton/index.tsx
+++ src/renderer/pages/HandleNote/components/ControlBox/components/CommitButton/index.tsx
@@ -1,17 +1,20 @@
 import { VFC, MouseEventHandler } from 'react';
 
+import { CSSObject } from '@emotion/react';
+
 import { 
   CommitIcon,
   NormalButtonAnimationWrapper
   } from '~/components';
 
 type Props = {
+  css?: CSSObject;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 };
 
-export const CommitButton: VFC<Props> = ({ onClick }) => {
+export const CommitButton: VFC<Props> = ({ onClick, ...rest }) => {
   return (
-    <NormalButtonAnimationWrapper onClick={onClick}>
+    <NormalButtonAnimationWrapper onClick={onClick} {...rest}>
       <CommitIcon />
     </NormalButtonAnimationWrapper>
   );
