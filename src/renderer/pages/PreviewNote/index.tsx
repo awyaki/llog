49f9adacda286a7746f6957e338e0764aad986ba
awyaki <awyakitaka@gmail.com>
2022-01-29 10:44:51 +0900

    Move content title to the above of the page

--- src/renderer/pages/PreviewNote/index.tsx
+++ src/renderer/pages/PreviewNote/index.tsx
@@ -1,27 +1,17 @@
 import { VFC  } from 'react';
 
+import { font } from '~/styleConfig';
+
 import { 
   CommitIcon,
   NormalButtonAnimationWrapper,
   EditIcon,
+  NoteMaybeWithTitleAndButtons
 } from '~/components';
 
 import { NotFoundPage } from '~/pages';
 
-import { makeFormalTimeString } from '~/utils';
-
-import { TagList, BlockList } from './components';
-
-import { Box } from '@chakra-ui/react';
-
-import { pageTitle } from '~/pages/style';
-
 import { usePreviewNote } from './hooks';
-
-import { 
-  noteStyle, 
-  dateStyle,
-  } from './style';
 
 import 'zenn-content-css';
 
@@ -29,29 +19,25 @@ export const PreviewNote: VFC = () => {
   const  { note, contentName, onCommitLog, onClickEdit } = usePreviewNote();
   if (note === null) return <NotFoundPage />;
 
-  const { updatedAt, tags, blocks, transformed } = note;
-
-  return (
-    <>
-      <h2 css={{ ...pageTitle, marginBottom: '8px' }}>{contentName}</h2>
-      <Box css={noteStyle}>
-        <div css={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div css={dateStyle}>{makeFormalTimeString(updatedAt)}</div>
-          <div css={{ display: 'flex' }}>
-            <NormalButtonAnimationWrapper 
-              onClick={onCommitLog}
-              css={{ marginRight: '8px' }}>
+  
+  const Buttons = [
+            <NormalButtonAnimationWrapper onClick={onCommitLog}>
               <CommitIcon />
-            </NormalButtonAnimationWrapper>
+            </NormalButtonAnimationWrapper>,
             <NormalButtonAnimationWrapper onClick={onClickEdit}>
                 <EditIcon />
             </NormalButtonAnimationWrapper>
-          </div>
-        </div>
-        <TagList tags={tags} />
-        <BlockList blocks={blocks} />
-        <div className="znc" dangerouslySetInnerHTML={{ __html: transformed }}></div>
-      </Box>
+  ];
+
+  const Note = NoteMaybeWithTitleAndButtons({ Buttons: Buttons });
+
+  return (
+    <>
+      <h1 css={{ 
+        fontSize: font.size.L,
+        marginBottom: '16px',
+        }}>{contentName}</h1>
+      <Note note={note} />
     </>
   );
 };
