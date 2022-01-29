75fc16080b96b022c889bdc88a39e091187cea80
awyaki <awyakitaka@gmail.com>
2022-01-29 13:29:03 +0900

    Add css prop

--- src/renderer/components/TagsController/SelectedTagsList/index.tsx
+++ src/renderer/components/TagsController/SelectedTagsList/index.tsx
@@ -1,5 +1,7 @@
 import { VFC, useContext } from 'react';
 
+import { CSSObject } from '@emotion/react';
+
 import { EditSelectedTagsButton } from '../EditSelectedTagsButton';
 
 import { CreateTagButton } from '../CreateTagButton';
@@ -11,10 +13,14 @@ import {
 
 import { SelectedTagsContext } from '../SelectedTagsContextProvider';
 
-export const SelectedTagsList: VFC = () => {
+type Props = {
+  css?: CSSObject;
+};
+
+export const SelectedTagsList: VFC<Props> = ({ ...rest }) => {
   const { selectedTags } = useContext(SelectedTagsContext);
   return (
-    <>
+    <div {...rest}>
       <ul css={tagsContainer}>
         <li><CreateTagButton /></li>
         <li><EditSelectedTagsButton /></li>
@@ -22,6 +28,6 @@ export const SelectedTagsList: VFC = () => {
       <ul css={tagsContainer}>
         {selectedTags.map(({ id, name }) => <li key={id}><div css={tagStyle}>{name}</div></li>)}
       </ul>
-    </>
+    </div>
   );
 };
