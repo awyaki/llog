0c833d7ddb6758698dba8b7d6e170b3a559cfdb4
awyaki <awyakitaka@gmail.com>
2022-01-30 09:46:30 +0900

    Adjust layout of menu button

--- src/renderer/components/Menu/index.tsx
+++ src/renderer/components/Menu/index.tsx
@@ -35,12 +35,12 @@ export const Menu: VFC = () => {
         position: 'sticky',
         display: 'flex',
         top: 0,
-        justifyContent: 'flex-start',
+        justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'column',
         width: '160px', 
         minWidth: '160px',
-        padding: '24px 0 16px 0', 
+        padding: '24px 0 48px 0', 
         backgroundColor: colors.cyan.DEFAULT,
         zIndex: 100,
       }}>
@@ -50,7 +50,6 @@ export const Menu: VFC = () => {
           justifyContent: 'flex-start',
           alignItems: 'center',
           padding: '0 0 16px 24px',
-          marginBottom: '48px',
         }}>
         <BackButton 
           css={{ marginRight: '16px' }}
@@ -58,9 +57,11 @@ export const Menu: VFC = () => {
         <ForwardButton 
           onClick={onClickForward} />
       </div>
-      <LogsButton onClick={onClickLogs} />
-      <TagsButton onClick={onClickTags} />
-      <HomeButton onClick={onClickHome} /> 
+      <div css={{ width: '100%' }}>
+        <LogsButton onClick={onClickLogs} />
+        <TagsButton onClick={onClickTags} />
+        <HomeButton onClick={onClickHome} /> 
+      </div>
     </motion.div>
   );
 };
