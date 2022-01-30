474298a96be655c97c28f63b5415bf812a57ed4e
awyaki <awyakitaka@gmail.com>
2022-01-30 17:15:04 +0900

    Adjust motion and style of Notifier

--- src/renderer/components/Notifier/index.tsx
+++ src/renderer/components/Notifier/index.tsx
@@ -1,10 +1,21 @@
-import { VFC, useContext, useEffect } from 'react';
+import { VFC, useContext, useEffect, useMemo } from 'react';
 
-import { container } from './style';
-
-import { Fade, Box } from '@chakra-ui/react';
+import { colors } from '~/styleConfig';
 
 import { NotifierContext } from '~/components';
+
+import {
+  motion,
+  Variants
+} from 'framer-motion';
+
+const motions: Variants = {
+  hidden: { opacity: 0 },
+  show: { 
+    opacity: [0, 1, 1, 0],
+    transition: { duration: 3, times: [0, 0.3, 0.7, 1] }
+    },
+};
 
 export const Notifier: VFC = () => {
   const { isShow, message, setMessage } = useContext(NotifierContext); 
@@ -19,12 +30,27 @@ export const Notifier: VFC = () => {
     
     return () => clearTimeout(timerId);
   }, [isShow]);
+  
 
   return (
-    <Fade in={isShow} unmountOnExit={true}>
-      <Box css={container}>
+    <motion.div 
+      variants={motions}
+      initial="hidden"
+      animate={isShow ? 'show' : 'hidden'}
+      style={{
+        minWidth: '150px',
+        backgroundColor: colors.cyan.DEFAULT,
+        color: colors.white,
+        boxShadow: '0 0 80px -10px rgba(0,0,0,0.50)',
+        borderRadius: '4px',
+        position: 'fixed',
+        top: '85vh',
+        left: '80vw',
+        padding: '20px',
+        zIndex: 3,
+        textAlign: 'center',
+      }}>
         {message}
-      </Box>
-    </Fade>
+    </motion.div>
   );
 };
