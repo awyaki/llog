7a8b2548c86916bff2cc51e2a7f49b983fc28c4c
awyaki <awyakitaka@gmail.com>
2022-02-03 22:31:58 +0900

    Create ModalToSubmitLogContextProvider

---
+++ src/renderer/pages/Logs/ModalToSubmitLogContextProvider.tsx
@@ -0,0 +1,29 @@
+import {
+  FC,
+  createContext
+  } from 'react';
+
+import { useDisclosure } from '@chakra-ui/react';
+
+type ProvidedModalToSubmitLogContext = {
+  isOpen: boolean,
+  onOpen: () => void;
+  onClose: () => void;
+};
+
+
+export const ModalToSubmitLogContext = createContext<ProvidedModalToSubmitLogContext>({
+  isOpen: false,
+  onOpen: () => {},
+  onClose: () => {},
+});
+
+export const ModalToSubmitLogContextProvider: FC = ({ children }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <ModalToSubmitLogContext.Provider value={{ isOpen, onOpen, onClose }}>
+      {children}
+    </ModalToSubmitLogContext.Provider>
+  );
+};
