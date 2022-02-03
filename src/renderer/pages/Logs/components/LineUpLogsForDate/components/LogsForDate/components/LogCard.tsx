0e80daf4005ab59091c7f92c805f4bfebfa6d69e
awyaki <awyakitaka@gmail.com>
2022-02-03 17:43:32 +0900

    Add Collapse to show content of logs

--- src/renderer/pages/Logs/components/LineUpLogsForDate/components/LogsForDate/components/LogCard.tsx
+++ src/renderer/pages/Logs/components/LineUpLogsForDate/components/LogsForDate/components/LogCard.tsx
@@ -1,4 +1,8 @@
-import { VFC } from 'react';
+import { 
+  VFC,
+  useState
+  } from 'react';
+
 
 
 import { colors, font } from '~/styleConfig';
@@ -12,8 +16,16 @@ import { Collapse } from '@chakra-ui/transition';
 
 import { 
   TagsList,
-  MiniBlockList
+  MiniBlockList,
+  BlockList,
+  NormalButton
   } from '~/components';
+
+import 'zenn-content-css';
+
+import { 
+  motion,
+  } from 'framer-motion';
 
 type Props = {
   log: LogWithRelation;
@@ -33,41 +45,65 @@ export const LogCard: VFC<Props> = ({ log }) => {
     contentId,
   } = log;
   
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleIsOopen = () => setIsOpen((prev) => !prev);
+
+
   console.log('LogCard', title);
   return (
-    <div
-      css={{
-        width: '100%',
-        borderLeftWidth: '8px',
-        borderStyle: 'solid',
-        padding: '16px',
-        backgroundColor: colors.cyan.BACKGROUND,
-        borderColor: colors.cyan.DEFAULT,
-        color: colors.text,
-      }}
-    >
-      <div css={{
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-      }}>
-        <div css={{ width: '90%' }}>
-          <div css={{
-            fontSize: font.size.SS,
-            marginBottom: '8px',
-          }}>
-            {makeFormalTimeString(createdAt)}
+    <>
+      <motion.button
+        onClick={toggleIsOopen}
+        style={{
+          width: '100%',
+          borderLeftWidth: '8px',
+          borderStyle: 'solid',
+          padding: '16px',
+          backgroundColor: colors.cyan.BACKGROUND,
+          borderColor: colors.cyan.DEFAULT,
+          color: colors.text,
+        }}
+      >
+        <div css={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+        }}>
+          <div css={{ width: '90%' }}>
+            <div css={{
+              fontSize: font.size.SS,
+              marginBottom: '8px',
+            }}>
+              {makeFormalTimeString(createdAt)}
+            </div>
+            <TagsList 
+              css={{ marginBottom: '4px' }}
+              tags={tags} />
+            <h2 css={{ fontSize: font.size.M }}>{title}</h2>
           </div>
-          <TagsList 
-            css={{ marginBottom: '4px' }}
-            tags={tags} />
-          <h2 css={{ fontSize: font.size.M }}>{title}</h2>
+          <MiniBlockList blocks={blocks} />
         </div>
-        <MiniBlockList blocks={blocks} />
-      </div>
-      
-      <div css={{ fontSize: font.size.SS, textAlign: 'end' }}>at {contentName}</div>
-    </div>
+        
+        <div css={{ fontSize: font.size.SS, textAlign: 'end' }}>at {contentName}</div>
+      </motion.button>
+      <Collapse in={isOpen}>
+        <div css={{
+          padding: '16px',
+          border: `1px solid ${colors.cyan.DEFAULT}`,
+          borderTopWidth: 0,
+          borderLeftWidth: '8px',
+          marginBottom: '16px',
+        }}>
+          <BlockList blocks={blocks} /> 
+          <div className="znc" dangerouslySetInnerHTML={{ __html: html }}></div>
+          
+        </div>
+        <NormalButton onClick={() => setIsOpen(false)}>
+          Close
+        </NormalButton>
+      </Collapse>
+    </>
   );
 };
