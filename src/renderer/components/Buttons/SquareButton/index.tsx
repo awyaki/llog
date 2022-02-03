e17fe69519fbc3c98c0edd0f595dd6ce190e4275
awyaki <awyakitaka@gmail.com>
2022-02-03 20:52:13 +0900

    Adjust behavior of the button

--- src/renderer/components/Buttons/SquareButton/index.tsx
+++ src/renderer/components/Buttons/SquareButton/index.tsx
@@ -7,33 +7,29 @@ import { colors } from '~/styleConfig';
 import {
   motion,
   Variants,
-  TargetAndTransition
 } from 'framer-motion';
 
 
 type ColorVariants = {
   primary: string,
-  secondry: string,
+  secondary: string,
 };
 
 const makeVariants = ({ 
   primary, 
-  secondry}: ColorVariants): Variants  => {
+  secondary}: ColorVariants): Variants  => {
 
     return {
 
       origin: {
-        x: 0,
-        scale: 1,
         borderColor: primary,
-        color: secondry,
+        color: secondary,
         backgroundColor: primary,
       },
 
       reverse: {
-        scale: 1.1,
         color: primary,
-        backgroundColor: secondry,
+        backgroundColor: secondary,
       },
     };
 
@@ -41,19 +37,23 @@ const makeVariants = ({
 
 type Props = {
   css?: CSSObject;
-  colorOption: ColorVariants;
+  colorOption?: ColorVariants;
+  onClick?: () => void;
   Icon: VFC<{ css?: CSSObject}>;
 };
 
 export const SquareButton: VFC<Props> = ({ 
   Icon, 
-  colorOption = { primary: colors.cyan.DEFAULT, secondry: colors.white },
+  colorOption = { primary: colors.cyan.DEFAULT, secondary: colors.white },
+  onClick,
   ...rest }) => {
 
   return (
     <motion.button
+      onClick={onClick}
       variants={makeVariants(colorOption)}
       initial="origin"
+      animate="origin"
       whileHover="reverse"
       style={{
         display: 'flex',
