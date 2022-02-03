3b24ebb2bb00ea505f049b51ce7c948968093486
awyaki <awyakitaka@gmail.com>
2022-02-03 10:51:01 +0900

    fix layout

--- src/renderer/components/NoteMaybeWithTitleAndButtons/index.tsx
+++ src/renderer/components/NoteMaybeWithTitleAndButtons/index.tsx
@@ -40,17 +40,19 @@ export const NoteMaybeWithTitleAndButtons: INoteMaybeWithTitleAndButtons = ({
       <div css={isNested ? ({ padding: '5px' }) : noteStyle} {...rest}>
         {title ? <h1 css={{ fontSize: font.size.M }}>{title}</h1> : undefined}
         <div css={{ display: 'flex', justifyContent: 'space-between', marginBottom: '16px', }}>
-          <div css={{
-            fontSize: font.size.SS,
-            color: colors.text,
-            marginBottom: '8px',
-          }}>{makeFormalTimeString(updatedAt)}</div>
+          <div>
+            <div css={{
+              fontSize: font.size.SS,
+              color: colors.text,
+              marginBottom: '8px',
+            }}>{makeFormalTimeString(updatedAt)}</div>
+            <TagsList css={{ marginBottom: '8px' }} tags={tags} />
+            <BlockList blocks={blocks} />
+          </div>
           <ul css={{ display: 'flex' }}>
             {Buttons?.map((Button, i) => <li key={i} css={{ marginRight: '8px' }}>{Button}</li>)}
           </ul>
         </div>
-        <TagsList css={{ marginBottom: '8px' }} tags={tags} />
-        <BlockList css={{ marginBottom: '16px' }} blocks={blocks} />
         <div className="znc" dangerouslySetInnerHTML={{ __html: transformed }}></div>
       </div>
   );
