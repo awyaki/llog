dde7e443954232275c6ad6bc78380823dd7384ba
awyaki <awyakitaka@gmail.com>
2022-02-07 21:30:40 +0900

    App onClickTag props to react click event on each tag

--- src/renderer/components/TagsController/TagsList/index.tsx
+++ src/renderer/components/TagsController/TagsList/index.tsx
@@ -18,10 +18,12 @@ import { motion, MotionProps } from 'framer-motion';
 type Props = {
   css?: CSSObject;
   secandary?: boolean;
+  onClickTag?: (id: number) => void;
   tags: Tag[];
 } & Pick<MotionProps, 'variants'>;
 
 export const TagsList: VFC<Props> = ({ 
+  onClickTag = () => {},
   variants,
   tags, 
   secandary,
@@ -32,23 +34,26 @@ export const TagsList: VFC<Props> = ({
       css={tagsContainer} 
       {...rest}>
       {tags.map(({ id, name }) => 
-        <motion.li 
-          key={id}
-          variants={variants}
-          style={{
-            minWidth: '80px',
-            textAlign: 'center',
-            fontSize: font.size.SS,
-            borderRadius: '4px',
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            padding: '2px 4px',
-            borderColor: colors.cyan.DEFAULT ,
-            backgroundColor: secandary ? colors.cyan.SECOND : 'transparent',
-            color: colors.cyan.DEFAULT,
-          }}>
-          {name}
-        </motion.li>)}
+        <li>
+          <motion.button
+            onClick={() => onClickTag(id)}
+            key={id}
+            variants={variants}
+            style={{
+              minWidth: '80px',
+              textAlign: 'center',
+              fontSize: font.size.SS,
+              borderRadius: '4px',
+              borderWidth: '1px',
+              borderStyle: 'solid',
+              padding: '2px 4px',
+              borderColor: colors.cyan.DEFAULT ,
+              backgroundColor: secandary ? colors.cyan.SECOND : 'transparent',
+              color: colors.cyan.DEFAULT,
+            }}>
+            {name}
+          </motion.button>
+        </li>)}
     </motion.ul>
   );
 };
