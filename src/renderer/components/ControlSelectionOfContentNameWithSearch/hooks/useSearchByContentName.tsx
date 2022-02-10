349ca9f4604ec0fd4f34f67991cd323e73c8f830
awyaki <awyakitaka@gmail.com>
2022-02-10 22:49:39 +0900

    Create logic of search contents by content names

---
+++ src/renderer/components/ControlSelectionOfContentNameWithSearch/hooks/useSearchByContentName.tsx
@@ -0,0 +1,100 @@
+import { Reducer, useReducer } from 'react';
+
+import { createNGramTokenMap } from '~/utils';
+
+export type ContentNameWithId = {
+  id: number;
+  contentName: string;
+};
+
+
+type SearchByContentNameState = {
+  contentNames: ContentNameWithId[];
+  searchContentNameQuery: string;
+  filteredContentNames: ContentNameWithId[];
+  tokenMapOfContentNames: Map<string, Set<number>>;
+};
+
+type SearchByContentNameAction = {
+  type: 'SEARCH_CONTENT_NAME/CALCULATE_WITH_NEW_CONTENT_NAMES';
+  contentNames: ContentNameWithId[];
+} | {
+  type: 'SEARCH_CONTENT_NAME/SET_SEARCH_CONTENT_NAME_QUERY';
+  searchContentNameQuery: string;
+};
+
+export const searchByContentNameRreducer: Reducer<
+  SearchByContentNameState,
+  SearchByContentNameAction
+> = (
+state,
+action
+) => {
+  switch (action.type) {
+    case 'SEARCH_CONTENT_NAME/CALCULATE_WITH_NEW_CONTENT_NAMES': {
+      const nextState = { ...state };
+      const nextContentNames = [...action.contentNames];
+
+      const nextTokenMapOfContentName = createNGramTokenMap(action.contentNames.map(({ id, contentName }) => ({ id, text: contentName })));
+      
+      nextState.contentNames = nextContentNames;
+      nextState.tokenMapOfContentNames = nextTokenMapOfContentName;
+
+      return nextState; 
+    }
+    
+    case 'SEARCH_CONTENT_NAME/SET_SEARCH_CONTENT_NAME_QUERY': {
+      const nextState = { ...state };
+      const nextSearchContentNameQuery = action.searchContentNameQuery;
+      const idSetOfContentName = state.tokenMapOfContentNames.get(action.searchContentNameQuery) ?? new Set();
+      
+      const nextFilteredContentNames = idSetOfContentName.size === 0
+                                        ? [...state.contentNames]
+                                        : state.contentNames.filter(({ id }) => idSetOfContentName.has(id));
+      
+      nextState.searchContentNameQuery = nextSearchContentNameQuery;
+      nextState.filteredContentNames = nextFilteredContentNames;
+
+      return nextState;
+    }
+
+    default:
+      return state;
+  }
+};
+
+type UseSearchByContentNameArgument = { 
+  contentNames: ContentNameWithId[]
+};
+
+const init = (contentNames: ContentNameWithId[]): SearchByContentNameState => {
+  const initialContentNames = [...contentNames];
+  const initialFilteredContentNames = [...contentNames];
+  const initialTokenMapOfContentNames = createNGramTokenMap(contentNames.map(({ id, contentName }) => ({ id, text: contentName })));
+
+  return {
+    contentNames: initialContentNames,
+    filteredContentNames: initialFilteredContentNames,
+    tokenMapOfContentNames: initialTokenMapOfContentNames,
+    searchContentNameQuery: '' 
+  };
+};
+
+export const useSearchByContentName = ({
+  contentNames
+  }: UseSearchByContentNameArgument) => {
+
+  const [{ filteredContentNames, searchContentNameQuery }, dispatch] = useReducer(searchByContentNameRreducer,  contentNames, init); 
+  
+  const setSearchContentNameQuery = (searchContentNameQuery: string) => {
+    dispatch({ type: 'SEARCH_CONTENT_NAME/SET_SEARCH_CONTENT_NAME_QUERY', searchContentNameQuery: searchContentNameQuery });
+  };
+  
+  return {
+    filteredContentNames,
+    searchContentNameQuery,
+    setSearchContentNameQuery,
+  };
+};
+
+
