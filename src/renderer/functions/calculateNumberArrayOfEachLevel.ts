3d2de9c2f70a52c5f21101b2899facf21d8a071d
awyaki <awyakitaka@gmail.com>
2022-02-12 19:55:47 +0900

    Create calculateNumberArrayOfEachLevel

---
+++ src/renderer/functions/calculateNumberArrayOfEachLevel.ts
@@ -0,0 +1,16 @@
+import { Block } from '@prisma/client';
+
+export const calculateNumberArrayOfEachLevel = (blocks: Block[]) => {
+  return blocks.map((block) => block.level)
+                            .reduce((acc, cur) => {
+                              const [l0, l1, l2, l3, l4, l5] = acc;
+                              if (cur === 0) return [l0 + 1, l1,     l2,     l3,     l4,     l5    ];
+                              if (cur === 1) return [l0,     l1 + 1, l2,     l3,     l4,     l5    ];
+                              if (cur === 2) return [l0,     l1,     l2 + 1, l3,     l4,     l5    ];
+                              if (cur === 3) return [l0,     l1,     l2,     l3 + 1, l4,     l5    ];
+                              if (cur === 4) return [l0,     l1,     l2,     l3,     l4 + 1, l5    ];
+                              if (cur === 5) return [l0,     l1,     l2,     l3,     l4,     l5 + 1];
+                              return acc;
+                            }, [0, 0, 0, 0, 0, 0]);
+
+};
