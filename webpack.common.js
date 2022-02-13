b40f439eccb70279b0c6d7fdeb78b57b8c29e87e
awyaki <awyakitaka@gmail.com>
2022-02-13 15:20:00 +0900

    Remove unneccesary file copy

--- webpack.common.js
+++ webpack.common.js
@@ -26,7 +26,6 @@ const mainConfig = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'node_modules/@prisma/engines/libquery_engine-darwin.dylib.node', to: path.resolve(__dirname, 'dist') },
         { from: 'prisma/schema.prisma', to: path.resolve(__dirname, 'dist') },
         { from: 'prisma/dev.db', to: path.resolve(__dirname, 'dist') }, // TODO: use https://www.npmjs.com/package/webpack-node-externals
       ]
