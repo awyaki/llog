e601fbe1ba31b097a155119213283df5576f1094
awyaki <awyakitaka@gmail.com>
2022-04-11 18:50:35 +0900

    Rewrite by using SideMenu

--- src/renderer/pages/Contents/components/AddContent/index.tsx
+++ src/renderer/pages/Contents/components/AddContent/index.tsx
@@ -1,8 +1,9 @@
 import { VFC } from 'react';
 
-import { colors } from '~/styleConfig';
-
-import { AddContentForm } from '~/components';
+import { 
+  AddContentForm,
+  SideMenu,
+  } from '~/components';
 
 
 type AddContentProps = {
@@ -15,34 +16,10 @@ export const AddContent: VFC<AddContentProps> = ({
   onClose,
   }) => {
   return (
-    <div css={{
-      position: 'relative',
-      width: isOpen ? '250px' : '0px',
-      height: '100%',
-      zIndex: 1,
-      transitionProperty: 'width',
-      transitionDuration: '.5s',
-      transitionTimingFunction: 'ease',
-    }}>
-      <div css={{ 
-          display: isOpen ? 'block' : 'none',
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          paddingLeft: '24px',
-          borderLeft: `1px solid ${colors.cyan.DEFAULT}`,
-          height: '100%',
-        }}>
-      <button 
-        css={{
-          width: '100%',
-          display: 'block',
-          textAlign: 'right',
-          marginBottom: '16px',
-        }}
-        onClick={onClose}>X</button>
-        <AddContentForm />
-      </div>
-    </div>
+    <SideMenu 
+      isOpen={isOpen}
+      onClose={onClose}>
+      <AddContentForm />
+    </SideMenu>
   );
 };
