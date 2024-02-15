736196aa7c1a3322174a5007c15aa24f03eeee81
awyaki <awyakitaka@gmail.com>
2024-02-15 20:12:24 +0900

    Adjust the maxWidth of TagList to fix the position of X button on ContentForm

--- src/renderer/components/TagsController/SelectTags/components/TagListToSelect/index.tsx
+++ src/renderer/components/TagsController/SelectTags/components/TagListToSelect/index.tsx
@@ -1,36 +1,27 @@
-import { 
-  VFC,
-  useContext,
-  useCallback,
-  Dispatch,
-  SetStateAction
-  } from 'react';
+import { VFC, useContext, useCallback, Dispatch, SetStateAction } from "react";
 
-import { colors, font } from '~/styleConfig';
+import { colors, font } from "~/styleConfig";
 
-import { deleteTag, getAllTag } from '~/api';
+import { deleteTag, getAllTag } from "~/api";
 
-import { Tag } from '@prisma/client';
+import { Tag } from "@prisma/client";
 
-import { CSSObject } from '@emotion/react';
+import { CSSObject } from "@emotion/react";
 
-import { SelectedTagsContext } from '~/components';
-
-
+import { SelectedTagsContext } from "~/components";
 
 const tagStyle: CSSObject = {
-  minWidth: '80px',
-  textAlign: 'center',
+  minWidth: "80px",
+  textAlign: "center",
   fontSize: font.size.SS,
-  borderRadius: '4px',
-  borderWidth: '1px',
-  borderStyle: 'solid',
+  borderRadius: "4px",
+  borderWidth: "1px",
+  borderStyle: "solid",
   borderColor: colors.cyan.DEFAULT,
   backgroundColor: colors.white,
   color: colors.cyan.DEFAULT,
-  padding: '2px 4px',
-  transition: '.25s',
-
+  padding: "2px 4px",
+  transition: ".25s",
 };
 
 const reverseTagStyle: CSSObject = {
@@ -38,7 +29,6 @@ const reverseTagStyle: CSSObject = {
   borderColor: colors.white,
   backgroundColor: colors.cyan.DEFAULT,
   color: colors.white,
-
 };
 
 type Props = {
@@ -48,20 +38,17 @@ type Props = {
   onToggleSelectedTags: (tag: Tag) => void;
 };
 
-export const TagListToSelect: VFC<Props> = ({ 
-  searchQuery, 
+export const TagListToSelect: VFC<Props> = ({
+  searchQuery,
   selectedTags,
   setSelectedTags,
   onToggleSelectedTags,
-  }) => {
+}) => {
+  const { filterTagsbyUserInput, setTagsAction } =
+    useContext(SelectedTagsContext);
 
-  const { 
-    filterTagsbyUserInput,
-    setTagsAction
-    } = useContext(SelectedTagsContext);
-  
-  const filteredTags = filterTagsbyUserInput(searchQuery); 
-  
+  const filteredTags = filterTagsbyUserInput(searchQuery);
+
   const deleteTagWithId = useCallback(async (id: number) => {
     await deleteTag(id);
     const newAlltags = await getAllTag();
@@ -72,28 +59,36 @@ export const TagListToSelect: VFC<Props> = ({
   }, []);
 
   return (
-    <ul css={{
-      width: '100%',
-      maxHeight: '400px',
-      overflowY: 'scroll',
-      display: 'flex',
-      flexWrap: 'wrap',
-      '> li': {
-        marginRight: '4px',
-        marginBottom: '4px',
-      },
-    }}>
+    <ul
+      css={{
+        width: "100%",
+        maxWidth: "200px",
+        maxHeight: "400px",
+        overflowY: "scroll",
+        display: "flex",
+        flexWrap: "wrap",
+        "> li": {
+          marginRight: "4px",
+          marginBottom: "4px",
+        },
+      }}
+    >
       {filteredTags.map(({ id, name }) => {
-                          const style = selectedTags.some((tag) => tag.id === id) ? reverseTagStyle : tagStyle
-                          return <li key={id}>
-                              <button 
-                                onClick={() => onToggleSelectedTags({ id, name })}
-                                css={{ ...style, marginRight: '4px' }}
-                              >{name}</button>
-                              <button onClick={() => deleteTagWithId(id)}>
-                                x
-                              </button>
-                            </li>})}
+        const style = selectedTags.some((tag) => tag.id === id)
+          ? reverseTagStyle
+          : tagStyle;
+        return (
+          <li key={id}>
+            <button
+              onClick={() => onToggleSelectedTags({ id, name })}
+              css={{ ...style, marginRight: "4px" }}
+            >
+              {name}
+            </button>
+            <button onClick={() => deleteTagWithId(id)}>x</button>
+          </li>
+        );
+      })}
     </ul>
   );
 };
