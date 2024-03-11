00e92d5bd106d7afccaafdd7ca3f8d61225fce47
awyaki <awyakitaka@gmail.com>
2024-03-11 16:35:33 +0900

    ビルド時に静的ファイルをコピーするように設定

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,8 +1,23 @@
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
+    plugins: [
+      externalizeDepsPlugin(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "prisma/dev.db",
+            dest: "./",
+          },
+          {
+            src: "icons/Icon-Llog.icns",
+            dest: "./",
+          },
+        ],
+      }),
+    ],
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
