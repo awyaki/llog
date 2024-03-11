5f5f26271a3248eead98fc082369b8384c7827a9
awyaki <awyakitaka@gmail.com>
2024-03-11 17:49:46 +0900

    静的ファイルのコピーのための設定を一時的に削除

--- electron.vite.config.js
+++ electron.vite.config.js
@@ -1,25 +1,16 @@
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 
 export default defineConfig({
   main: {
-    plugins: [
-      externalizeDepsPlugin(),
-      viteStaticCopy({
-        targets: [
-          {
-            src: "prisma/dev.db",
-            dest: "./",
-          },
-          {
-            src: "icons/Icon-Llog.icns",
-            dest: "./",
-          },
-        ],
-      }),
-    ],
+    build: {
+      outDir: "./out",
+    },
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
+    build: {
+      outDir: "./out",
+    },
     plugins: [externalizeDepsPlugin()],
   },
 });
