c0cdda4d0ec30cd893107437de75f5d7e8cb8d83
awyaki <awyakitaka@gmail.com>
2024-03-17 16:56:36 +0900

    LLog初回起動時やDBファイルがないとき最新のマイグレーションデータでマイグレーションを行う
    * see https://github.com/awohletz/electron-prisma-trpc-example/blob/main/src/server/prisma.ts

--- src/main/db/db.ts
+++ src/main/db/db.ts
@@ -1,10 +1,11 @@
+import log from "electron-log";
+import { dbUrl, migrationEnginePath, queryEnginePath } from "./constants";
 import path from "node:path";
-import { app } from "electron";
 import { PrismaClient } from "@prisma/client";
+import { fork } from "node:child_process";
 
-const dbUrl = app.isPackaged
-  ? `file:${path.join(app.getPath("userData"), "llog.db")}`
-  : "file:./dev.db";
+log.info("DB URL", dbUrl);
+log.info("Qery Engine Path", queryEnginePath);
 
 export const prisma = new PrismaClient({
   datasources: {
@@ -12,4 +13,67 @@ export const prisma = new PrismaClient({
       url: dbUrl,
     },
   },
+  // see https://github.com/prisma/prisma/discussions/5200
+  // @ts-expect-error internal prop
+  __internal: {
+    engine: { binaryPath: queryEnginePath },
+  },
 });
+
+export const runPrismaCommand = async ({
+  command,
+  dbUrl,
+}: {
+  command: string[];
+  dbUrl: string;
+}): Promise<number> => {
+  log.info("Migration engine path", migrationEnginePath);
+  log.info("Query engine path", queryEnginePath);
+
+  try {
+    const exitCode = await new Promise((resolve) => {
+      const prismaPath = path.resolve(
+        __dirname,
+        "..",
+        "..",
+        "node_modules/prisma/build/index.js",
+      );
+      log.info("Prisma path", prismaPath);
+
+      const child = fork(prismaPath, command, {
+        env: {
+          ...process.env,
+          DATABASE_URL: dbUrl,
+          PRISMA_MIGRATION_ENGINE_BINARY: migrationEnginePath,
+          PRISMA_QUERY_ENGINE_LIBRARY: queryEnginePath,
+
+          // Prisma apparently needs a valid path for the format and introspection binaries, even though
+          // we don't use them. So we just point them to the query engine binary. Otherwise, we get
+          // prisma:  Error: ENOTDIR: not a directory, unlink '/some/path/electron-prisma-trpc-example/packed/mac-arm64/ElectronPrismaTrpcExample.app/Contents/Resources/app.asar/node_modules/@prisma/engines/prisma-fmt-darwin-arm64'
+          PRISMA_FMT_BINARY: queryEnginePath,
+          PRISMA_INTROSPECTION_ENGINE_BINARY: queryEnginePath,
+        },
+        stdio: "pipe",
+      });
+
+      child.on("message", (msg) => {
+        log.info(msg);
+      });
+
+      child.on("close", (code) => {
+        resolve(code);
+      });
+
+      child.stderr?.on("data", (data) => {
+        log.error("prisma: ", data.toString());
+      });
+    });
+
+    if (exitCode !== 0)
+      throw Error(`command ${command} failed with exit code ${exitCode}`);
+    return exitCode;
+  } catch (e) {
+    log.error(e);
+    throw e;
+  }
+};
--- src/main/main.ts
+++ src/main/main.ts
@@ -1,9 +1,12 @@
+import fs from "node:fs";
 import path from "path";
 import { app, BrowserWindow, shell } from "electron";
-
+import log from "electron-log";
 import { useDBQueryOnClient } from "./db";
+import { prisma, runPrismaCommand } from "./db/db";
 
 import { useMarkdownToHTML } from "./useMarkdownToHTML";
+import { dbUrl, dbPath, type Migration, latestMigration } from "./db/constants";
 
 useDBQueryOnClient();
 useMarkdownToHTML();
@@ -13,7 +16,46 @@ const handleURLOpen = (e: Electron.Event, url: string) => {
   shell.openExternal(url);
 };
 
-function createWindow() {
+const createWindow = async () => {
+  // 必要があればデータベースのマイグレーションを行う
+  let needsMigration;
+  const dbExists = fs.existsSync(dbPath);
+  if (!dbExists) {
+    needsMigration = true;
+    fs.closeSync(fs.openSync(dbPath, "w"));
+  } else {
+    try {
+      const latest: Migration[] =
+        await prisma.$queryRaw`select * from _prisma_migrations order by finished_at`;
+      needsMigration =
+        latest[latest.length - 1]?.migration_name !== latestMigration;
+    } catch (e) {
+      log.error(e);
+      needsMigration = true;
+    }
+  }
+
+  if (needsMigration) {
+    try {
+      const schemaPath = path.join(
+        app.getAppPath().replace("app.asar", "app.asar.unpacked"),
+        "prisma",
+        "schema.prisma",
+      );
+
+      await runPrismaCommand({
+        command: ["migrate", "deploy", "--schema", schemaPath],
+        dbUrl,
+      });
+      log.info("Migration done.");
+    } catch (e) {
+      log.error(e);
+      process.exit(1);
+    }
+  } else {
+    log.info("Does not need migration");
+  }
+
   const win = new BrowserWindow({
     width: 1200,
     height: 600,
@@ -28,7 +70,7 @@ function createWindow() {
 
   win.webContents.on("will-navigate", handleURLOpen);
   win.webContents.on("new-window", handleURLOpen);
-}
+};
 
 app.whenReady().then(createWindow);
 
